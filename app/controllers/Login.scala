feb1511eff1faaaabb23c861530a6e7dd0cf52b3
Rainer Simon <rainer.simon@ait.ac.at>
2016-01-22 10:23:01 +0100

    Basic form handling example

--- app/controllers/Login.scala
+++ app/controllers/Login.scala
@@ -10,7 +10,7 @@ import play.api.data.Forms._
 import play.api.Play.current
 import play.api.i18n.Messages.Implicits._
 
-case class LoginData(name: String, email: String, password: String)
+case class LoginData(name: String, password: String)
 
 /** Just temporary container for everything while we're still hacking on the basics **/
 class Login @Inject() (implicit db: DB) extends Controller {
@@ -18,20 +18,27 @@ class Login @Inject() (implicit db: DB) extends Controller {
   val loginForm = Form(
     mapping(
       "name" -> nonEmptyText,
-      "email" -> email,
       "password" -> nonEmptyText
     )(LoginData.apply)(LoginData.unapply)
   )
 
- def showLoginForm = Action {
+  def showLoginForm = Action {
     Ok(views.html.login(loginForm))
   }
 
-  def processLogin = Action(parse.form(loginForm)) { implicit request =>
-    // TODO error handling
-    val loginData = request.body
-    Logger.info(loginData.toString)
-    Ok(views.html.landingPage())
+  def processLogin = Action { implicit request =>
+    loginForm.bindFromRequest.fold(
+      formWithErrors => {
+        Logger.info("Bad request!")
+        BadRequest(views.html.login(formWithErrors))
+      },
+
+      loginData => {
+        val loginData = request.body
+        Logger.info(loginData.toString)
+        Redirect(routes.Application.landingPage()) // .flashing("success" -> "Contact saved!")
+      }
+    )
   }
 
 }
