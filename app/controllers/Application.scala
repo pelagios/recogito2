9783c4b1599138ea63384ccd0bf259ae8967395c
Rainer Simon <rainer.simon@ait.ac.at>
2016-01-22 09:06:31 +0100

    Created dummy pages for landing/login/signup

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,10 +6,19 @@ import javax.inject.Inject
 import play.api.mvc.{ Action, Controller }
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
+/** Just temporary container for everything while we're still hacking on the basics **/
 class Application @Inject() (implicit db: DB) extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def landingPage = Action {
+    Ok(views.html.landingPage())
+  }
+
+  def login = Action {
+    Ok(views.html.login())
+  }
+
+  def createAccount = Action {
+    Ok(views.html.signUp())
   }
 
   def listUsers = Action.async {
