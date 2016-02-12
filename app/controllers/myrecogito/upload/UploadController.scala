54fb9b31b61facab34bb6027c90365ca453cb97d
Rainer Simon <rainer.simon@ait.ac.at>
2016-02-12 09:56:04 +0100

    Upload: minimal views

--- app/controllers/myrecogito/upload/UploadController.scala
+++ app/controllers/myrecogito/upload/UploadController.scala
@@ -11,24 +11,36 @@ import play.api.Logger
 
 class UploadController @Inject() (implicit val db: DB, system: ActorSystem) extends AbstractController with AuthElement with Security {
 
+  def showStep1 = StackAction(AuthorityKey -> Normal) { implicit request =>
+    Ok(views.html.myrecogito.upload.upload_1())
+  }
+
+  def showStep2 = StackAction(AuthorityKey -> Normal) { implicit request =>
+    Ok(views.html.myrecogito.upload.upload_2())
+  }
+
+  def showStep3 = StackAction(AuthorityKey -> Normal) { implicit request =>
+    Ok(views.html.myrecogito.upload.upload_3())
+  }
+
   def processContentUpload = AsyncStack(AuthorityKey -> Normal) {  implicit request =>
     request.body.asMultipartFormData match {
-      
+
       case Some(formData) => Future.successful {
         formData.file("file") match {
           case Some(filePart) => {
               new GeoParser().parseAsync(filePart.ref.file)
               Ok("")
             }
-          case None => 
+          case None =>
             BadRequest("Form data missing")
         }
       }
-      
-      case None => 
+
+      case None =>
         Future.successful(BadRequest("Form data missing"))
-      
-    }    
+
+    }
   }
-  
+
 }
