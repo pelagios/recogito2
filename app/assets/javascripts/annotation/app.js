bd3013b16212cb195c2c56bd5e45706024f5eae1
Rainer Simon <rainer.simon@ait.ac.at>
2016-03-08 15:02:03 +0100

    Text annotation UI: sticky toolbar

--- app/assets/javascripts/annotation/app.js
+++ app/assets/javascripts/annotation/app.js
@@ -1,8 +1,21 @@
 require(['annotationRenderer', 'highlighter', 'selectionHandler'], function(AnnotationRenderer, Highlighter, SelectionHandler) {
 
   jQuery(document).ready(function() {
-    var highlighter = new Highlighter();
+    var highlighter = new Highlighter(),
 
+        toolbar = jQuery('.header-toolbar'),
+
+        makeToolbarSticky = function() {
+          jQuery(window).scroll(function() {
+            var scrollTop = jQuery(window).scrollTop();
+            if (scrollTop > 167)
+              toolbar.addClass('fixed');
+            else
+              toolbar.removeClass('fixed');
+          });
+        };
+
+    makeToolbarSticky();
     rangy.init();
 
     // Load annotations
