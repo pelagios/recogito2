129dc6e81c10e0ac6b94b8d22aee3cfbad2e44c0
Rainer Simon <rainer.simon@ait.ac.at>
2016-03-16 12:32:38 +0100

    Added test content for image tiling

---
+++ app/controllers/my/upload/tiling/TilingService.scala
@@ -0,0 +1,5 @@
+package controllers.my.upload.tiling
+
+class TilingService {
+  
+}
---
+++ test/controllers/my/upload/tiling/TilingServiceSpec.scala
@@ -0,0 +1,21 @@
+package controllers.my.upload.tiling
+
+import org.specs2.mutable._
+import org.specs2.runner._
+
+import org.junit.runner._
+import play.api.test._
+import play.api.test.Helpers._
+
+@RunWith(classOf[JUnitRunner])
+class TilingServiceSpec extends Specification {
+  
+  "The Tiling function" should {
+    
+    "tile the test image" in {
+      success
+    }
+    
+  }
+  
+}
