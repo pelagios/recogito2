ef86346ea09a229172d80b3895dd0c5917da69cf
Rainer Simon <rainer.simon@ait.ac.at>
2016-03-16 13:07:22 +0100

    Tile generation command - unit test

--- app/controllers/my/upload/tiling/TilingService.scala
+++ app/controllers/my/upload/tiling/TilingService.scala
@@ -1,5 +1,22 @@
 package controllers.my.upload.tiling
 
-class TilingService {
+import java.io.File
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import scala.concurrent.Future
+import scala.language.postfixOps
+import sys.process._
+
+object TilingService {
+  
+  private[tiling] def createZoomify(file: File, destFolder: File): Future[Unit] = {
+    Future {
+      s"vips dzsave $file $destFolder --layout zoomify" !
+    } map { result =>
+      if (result == 0)
+        Unit
+      else
+        throw new Exception("Image tiling failed for " + file.getAbsolutePath + " to " + destFolder.getAbsolutePath)
+    }
+  }
   
 }
--- test/controllers/my/upload/tiling/TilingServiceSpec.scala
+++ test/controllers/my/upload/tiling/TilingServiceSpec.scala
@@ -1,18 +1,44 @@
 package controllers.my.upload.tiling
 
+import java.io.File
+import org.apache.commons.io.FileUtils
 import org.specs2.mutable._
 import org.specs2.runner._
-
 import org.junit.runner._
 import play.api.test._
 import play.api.test.Helpers._
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 @RunWith(classOf[JUnitRunner])
 class TilingServiceSpec extends Specification {
   
+  val TEST_IMAGE = new File("test/resources/Ptolemy_map_15th_century.jpg")
+  
+  val TMP_DIR = {
+    val dir = new File("test/resources/tmp")
+    if (dir.exists)
+      FileUtils.deleteDirectory(dir)
+    dir
+  }
+  
   "The Tiling function" should {
     
-    "tile the test image" in {
+    "create proper Zoomify tiles from the test image" in {
+      Await.result(TilingService.createZoomify(TEST_IMAGE, TMP_DIR), 10 seconds)
+      
+      TMP_DIR.exists must equalTo(true)
+      TMP_DIR.list.size must equalTo(2)
+      new File(TMP_DIR, "ImageProperties.xml").exists must equalTo(true)
+      
+      val tileGroup0 = new File(TMP_DIR, "TileGroup0")
+      tileGroup0.exists must equalTo(true)
+      
+      tileGroup0.list.size must equalTo(65)
+      tileGroup0.list.filter(_.endsWith(".jpg")).size must equalTo(65)
+      
+      FileUtils.deleteDirectory(TMP_DIR)
+      
       success
     }
     
