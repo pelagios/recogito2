6ad119454884178ae9645cebd450bb833b54a96f
Rainer Simon <rainer.simon@ait.ac.at>
2016-03-25 14:34:58 +0100

    Minor changes to test place JSON

--- test/models/place/PlaceSpec.scala
+++ test/models/place/PlaceSpec.scala
@@ -3,6 +3,7 @@ package models.place
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
+import play.api.Logger
 import play.api.test._
 import play.api.test.Helpers._
 import org.joda.time.format.DateTimeFormat
@@ -18,7 +19,9 @@ class PlaceSpec extends Specification {
     
     "be properly created from JSON" in {
       val json = Source.fromFile("test/resources/place.json").getLines().mkString("\n")
-      val result = Json.fromJson[Place](Json.parse(json))
+      val place = Json.fromJson[Place](Json.parse(json))
+      
+      Logger.info(place.toString)
       
       success
     }
