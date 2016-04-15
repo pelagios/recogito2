7adcd535215c2d4a5c2eaa9f7608f0a2f133fe4e
Rainer Simon <rainer.simon@ait.ac.at>
2016-04-15 13:49:31 +0200

    Text annotation editor: place section

--- app/assets/javascripts/annotation/text/editor/editor.js
+++ app/assets/javascripts/annotation/text/editor/editor.js
@@ -1,34 +1,44 @@
-define(['editor/commentsection'], function(CommentSection) {
+define(['editor/placesection', 'editor/commentsection'], function(PlaceSection, CommentSection) {
 
   /** The main annotation editor popup **/
   var Editor = function() {
 
-    var element = jQuery(
-          '<div class="text-annotation-editor">' +
-            '<div class="handle"></div>' +
-            '<div class="category-buttons">' +
-              '<div class="category place">' +
-                '<span class="icon">&#xf041;</span> Mark as Place' +
+    var element = (function() {
+          var el = jQuery(
+            '<div class="text-annotation-editor">' +
+              '<div class="handle"></div>' +
+              '<div class="category-buttons">' +
+                '<div class="category place">' +
+                  '<span class="icon">&#xf041;</span> Mark as Place' +
+                '</div>' +
+                '<div class="category person">' +
+                  '<span class="icon">&#xf007;</span> Mark as Person' +
+                '</div>' +
+                '<div class="category event">' +
+                  '<span class="icon">&#xf005;</span> Mark as Event' +
+                '</div>' +
               '</div>' +
-              '<div class="category person">' +
-                '<span class="icon">&#xf007;</span> Mark as Person' +
+              '<div class="bodies"></div>' +
+              '<div class="footer">' +
+                '<button class="btn small outline cancel">Cancel</button>' +
+                '<button class="btn small ok">OK</button>' +
               '</div>' +
-              '<div class="category event">' +
-                '<span class="icon">&#xf005;</span> Mark as Event' +
-              '</div>' +
-            '</div>' +
-            '<div class="bodies"></div>' +
-            '<div class="footer">' +
-              '<button class="btn small outline cancel">Cancel</button>' +
-              '<button class="btn small ok">OK</button>' +
-            '</div>' +
-          '</div>'),
+            '</div>');
+
+          jQuery(document.body).append(el);
+          el.draggable({ handle: '.handle' });
+          return el;
+        })(),
 
         btnCancel = element.find('button.cancel'),
         btnOk = element.find('button.ok'),
 
-        commentSection = new CommentSection(element.find('.bodies')),
+        bodies = element.find('.bodies'),
 
+        // Just for testing & styling
+        dummyPlaceSection = new PlaceSection(bodies),
+
+        commentSection = new CommentSection(bodies),
 
         show = function() {
           element.show();
@@ -39,8 +49,7 @@ define(['editor/commentsection'], function(CommentSection) {
         };
 
     // hide();
-    jQuery(document.body).append(element);
-    element.draggable({ handle: '.handle' });
+
 
     // TODO just a dummy
     btnCancel.click(hide);
--- app/assets/javascripts/annotation/text/editor/placesection.js
+++ app/assets/javascripts/annotation/text/editor/placesection.js
@@ -1,12 +1,34 @@
 define([], function() {
 
-  /** The main annotation editor popup **/
-  var PlaceBody = function() {
+  var PlaceSection = function(parent) {
+    var element = (function() {
+          var el = jQuery(
+            '<div class="section place">' +
+              '<div class="map"></div>' +
+              '<div class="panel-container">' +
+                '<div class="panel"></div>' +
+              '</div>' +
+            '</div>');
 
-    // var element = jQuery();
+          parent.append(el);
+          return el;
+        })(),
 
+        awmc = L.tileLayer('http://a.tiles.mapbox.com/v3/isawnyu.map-knmctlkh/{z}/{x}/{y}.png', {
+          attribution: 'Tiles &copy; <a href="http://mapbox.com/" target="_blank">MapBox</a> | ' +
+                       'Data &copy; <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, CC-BY-SA | '+
+                       'Tiles and Data &copy; 2013 <a href="http://www.awmc.unc.edu" target="_blank">AWMC</a> ' +
+                       '<a href="http://creativecommons.org/licenses/by-nc/3.0/deed.en_US" target="_blank">CC-BY-NC 3.0</a>'
+        }),
+
+        map = new L.Map(element.find('.map')[0], {
+          center: new L.LatLng(41.893588, 12.488022),
+          zoom: 3,
+          zoomControl: false,
+          layers: [ awmc ]
+        });
   };
 
-  return PlaceBody;
+  return PlaceSection;
 
 });
