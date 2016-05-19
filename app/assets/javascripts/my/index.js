5ccf30c1c4931603dbd1a9f89f693f8f80b41ccd
Rainer Simon <rainer.simon@ait.ac.at>
2016-05-19 14:01:19 +0200

    Document delete in Personal Index

--- app/assets/javascripts/my/index.js
+++ app/assets/javascripts/my/index.js
@@ -1,11 +1,14 @@
 require([], function() {
 
   jQuery(document).ready(function() {
-    var documentPane = jQuery('.document-pane'),
+        /** Trashcan icon **/
+    var trashcan = jQuery('button.delete'),
 
+        /** Document elements **/
         documents = jQuery('.document'),
 
-        getDocumentEl = function(e) {
+        /** Resolves the click target to the parent document element **/
+        getClickedDocument = function(e) {
           var docEl = jQuery(e.target).closest('.document');
           if (docEl.length > 0)
             return docEl;
@@ -15,38 +18,73 @@ require([], function() {
 
         deselectAll = function() {
           documents.removeClass('selected');
+          trashcan.addClass('disabled');
         },
 
+        getSelectedDocumentIDs = function() {
+          var selected = jQuery.grep(documents, function(docEl) {
+            return jQuery(docEl).hasClass('selected');
+          });
+
+          return jQuery.map(selected, function(el) {
+            return el.dataset.id;
+          });
+        },
+
+        /**
+         * Global click handler on the document, so we can
+         * de-select if user clicks anywhere on the page
+         */
         onClick = function(e) {
-          var doc = getDocumentEl(e);
+          var trashcanClicked = e.target === trashcan[0],
+              doc = getClickedDocument(e);
 
-          if (doc) {
-            if (!e.ctrlKey)
-              deselectAll();
-
-            doc.addClass('selected');
+          if (trashcanClicked) {
+            deleteDocuments(getSelectedDocumentIDs());
           } else {
-            // Click was outside the document list
-            deselectAll();
+            if (doc) {
+              if (!e.ctrlKey)
+                deselectAll();
+
+              trashcan.removeClass('disabled');
+              doc.addClass('selected');
+            } else {
+              // Click was outside the document list
+              deselectAll();
+            }
+          }
+        },
+
+        /** Deletes documents sequentially **/
+        deleteDocuments = function(ids) {
+          var head, tail;
+
+          if (ids.length > 0) {
+            head = ids[0];
+            tail = ids.slice(1);
+
+            jsRoutes.controllers.document.DocumentController.deleteDocument(head).ajax()
+              .fail(function(error) {
+                console.log(error);
+              })
+              .done(function(result) {
+                deleteDocuments(tail);
+              });
+          } else {
+            window.location.reload(true);
           }
         },
 
         openDocument = function(e) {
-          window.location.href = getDocumentEl(e).data('href');
-        },
+          var id = getClickedDocument(e).data('id'),
+              url = jsRoutes.controllers.document.annotation.AnnotationController
+                      .showAnnotationViewForDocPart(id, 1).absoluteURL();
 
-        deleteDocument = function(id) {
-          jsRoutes.controllers.document.DocumentController.deleteDocument(id).ajax()
-                  .done(function(result) {
-                    window.location.reload(true);
-                  })
-                  .fail(function(error) {
-                    console.log(error);
-                  });
+          window.location.href = url;
         };
 
     jQuery(document).click(onClick);
-    documentPane.on('dblclick', '.document', openDocument);
+    jQuery('.document-panel').on('dblclick', '.document', openDocument);
   });
 
 });
