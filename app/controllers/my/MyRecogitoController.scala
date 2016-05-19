6940a77b3581386273b56ef423089511bc7defea
Rainer Simon <rainer.simon@ait.ac.at>
2016-05-19 09:30:27 +0200

    Minor edits + made upload quota a config parameter

--- app/controllers/my/MyRecogitoController.scala
+++ app/controllers/my/MyRecogitoController.scala
@@ -5,12 +5,16 @@ import javax.inject.Inject
 import models.user.Roles._
 import models.user.UserService
 import models.document.DocumentService
+import play.api.Play
 import play.api.cache.CacheApi
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import scala.concurrent.Future
 import storage.DB
 
 class MyRecogitoController @Inject() (implicit val cache: CacheApi, val db: DB) extends BaseController {
+  
+  // TODO this may depend on user in the future
+  private lazy val QUOTA = Play.current.configuration.getInt("recogito.upload.quota").getOrElse(200)
 
   def my = StackAction(AuthorityKey -> Normal) { implicit request =>
     Redirect(routes.MyRecogitoController.index(loggedIn.user.getUsername))
@@ -21,7 +25,7 @@ class MyRecogitoController @Inject() (implicit val cache: CacheApi, val db: DB) 
     if (loggedInUser == usernameInPath) {
       // Personal space
       DocumentService.findByOwner(loggedInUser).map(documents => {
-        Ok(views.html.my.index(loggedIn.user, UserService.getUsedDiskspaceKB(loggedInUser), documents))
+        Ok(views.html.my.index(loggedIn.user, UserService.getUsedDiskspaceKB(loggedInUser), QUOTA, documents))
       })
     } else {
       // TODO show public profile instead
