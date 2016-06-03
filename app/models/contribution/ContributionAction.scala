dc3edeeebef5e0e94375c81a22753505b7d08ba9
Rainer Simon <rainer.simon@ait.ac.at>
2016-06-03 21:48:22 +0200

    Minor edits

--- app/models/contribution/ContributionAction.scala
+++ app/models/contribution/ContributionAction.scala
@@ -7,13 +7,14 @@ import play.api.libs.functional.syntax._
 object ContributionAction extends Enumeration {
   
   val CREATE_DOCUMENT        = Value("CREATE_DOCUMENT")
-  val EDIT_DOCUMENT_METADATA = Value("UPDATE_DOCUMENT")
+  val EDIT_DOCUMENT_METADATA = Value("EDIT_DOCUMENT_METADATA")
   val DELETE_DOCUMENT        = Value("DELETE_DOCUMENT")
   val SHARE_DOCUMENT         = Value("SHARE_DOCUMENT")
 
   val CREATE_BODY            = Value("CREATE_BODY")
   val EDIT_BODY              = Value("EDIT_BODY")
   val CONFIRM_BODY           = Value("CONFIRM_BODY")
+  val FLAG_BODY              = Value("FLAG_BODY")
   val DELETE_BODY            = Value("DELETE_BODY")
   
   // TODO forum posts
