91a8eb4fed2fe0118e0cd8dcc5c14ea6fb920226
Rainer Simon <rainer.simon@ait.ac.at>
2016-06-13 21:20:04 +0200

    Bugfix

--- app/assets/javascripts/common/helpers/formatting.js
+++ app/assets/javascripts/common/helpers/formatting.js
@@ -1,5 +1,33 @@
 define(function() {
+
+  // TODO we should put this on the server, where we can feed it from the DB
+  var KNOWN_GAZETTEERS = [
+        { prefix: 'pleiades', url_patterns: [ 'http://pleiades.stoa.org/places/' ] },
+        { prefix: 'dare', url_patterns: [ 'http://dare.ht.lu.se/places/' ] }
+      ];
+
   return {
+
+    /** Shortens a gazetteer URI to a shortcode **/
+    formatGazetteerURI: function(uri) {
+      var shortCode = uri;
+
+      jQuery.each(KNOWN_GAZETTEERS, function(i, g) {
+        var cont = true;
+
+        jQuery.each(g.url_patterns, function(j, pattern) {
+          if (uri.indexOf(pattern) === 0) {
+            shortCode = g.prefix + ':' + uri.substring(pattern.length);
+            cont = false;
+            return cont;
+          }
+        });
+
+        return cont;
+      });
+
+      return '<a class="gazetteer-id" href="' + uri + '" target="_blank">' + shortCode + '</a>';
+    },
 
     /** Formats a yyyyMMddToYear to YYYY [Era] **/
     yyyyMMddToYear: function(str) {
