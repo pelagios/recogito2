4a44ad1457aa2c113f1a0158c3889f50d008eb7c
Rainer Simon <rainer.simon@ait.ac.at>
2016-06-14 14:52:24 +0200

    Added skeleton 'public profile' page (issue #83)

--- app/controllers/my/MyRecogitoController.scala
+++ app/controllers/my/MyRecogitoController.scala
@@ -1,35 +1,58 @@
 package controllers.my
 
-import controllers.BaseController
 import javax.inject.Inject
-import models.user.Roles._
 import models.user.UserService
 import models.document.DocumentService
 import play.api.Play
 import play.api.cache.CacheApi
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import scala.concurrent.Future
 import storage.DB
+import play.api.mvc.Controller
+import controllers.{ HasCache, HasDatabase, Security }
+import jp.t2v.lab.play2.auth.OptionalAuthElement
 
-class MyRecogitoController @Inject() (implicit val cache: CacheApi, val db: DB) extends BaseController {
+class MyRecogitoController @Inject() (implicit val cache: CacheApi, val db: DB) 
+  extends Controller with HasCache with HasDatabase with OptionalAuthElement with Security {
   
   // TODO this may depend on user in the future
   private lazy val QUOTA = Play.current.configuration.getInt("recogito.upload.quota").getOrElse(200)
 
-  def my = StackAction(AuthorityKey -> Normal) { implicit request =>
-    Redirect(routes.MyRecogitoController.index(loggedIn.user.getUsername))
+  /** A generic /my route that always redirects to the personal index **/
+  def my = StackAction { implicit request =>
+    loggedIn match {
+      case Some(userWithRoles) =>
+        Redirect(routes.MyRecogitoController.index(userWithRoles.user.getUsername))
+        
+      case None =>
+        // Not logged in - go to log in and then come back here
+        Redirect(controllers.landing.routes.LoginLogoutController.showLoginForm)
+          .withSession("access_uri" -> routes.MyRecogitoController.my.url)
+    }
   }
   
-  def index(usernameInPath: String) = AsyncStack(AuthorityKey -> Normal) { implicit request =>
-    val loggedInUser = loggedIn.user.getUsername
-    if (loggedInUser == usernameInPath) {
+  def index(usernameInPath: String) = AsyncStack { implicit request =>
+    // If the user is logged in & the name in the path == username it's the profile owner
+    val isProfileOwner = loggedIn match {
+      case Some(userWithRoles) => userWithRoles.user.getUsername.equalsIgnoreCase(usernameInPath)
+      case None => false
+    }
+      
+    if (isProfileOwner) {
       // Personal space
-      DocumentService.findByOwner(loggedInUser).map(documents => {
-        Ok(views.html.my.index(loggedIn.user, UserService.getUsedDiskspaceKB(loggedInUser), QUOTA, documents))
-      })
+      val username = loggedIn.get.user.getUsername
+      DocumentService.findByOwner(username).map(documents =>
+        Ok(views.html.my.index_private(loggedIn.get.user, UserService.getUsedDiskspaceKB(username), QUOTA, documents)))
     } else {
-      // TODO show public profile instead
-      Future.successful(Forbidden)
+      // Public profile
+      UserService.findByUsername(usernameInPath).map(_ match {
+        case Some(userWithRoles) =>
+          // Show public profile
+          Ok(views.html.my.index_public(userWithRoles.user))
+              
+        case None =>
+          // There is no user with the specified name
+          NotFound
+      })
     }
   }
 
