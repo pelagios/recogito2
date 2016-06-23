11efe659f3cf6a625597a2e5db505a29c1a27d97
Rainer Simon <rainer.simon@ait.ac.at>
2016-06-23 16:56:54 +0200

    JS asset compile & concat

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,14 @@
 require.config({
-  baseUrl: "/assets/javascripts",
-  fileExclusionRegExp: /^lib$/
+  baseUrl : "/assets/javascripts",
+  fileExclusionRegExp : /^lib$/,
+  modules : [
+    { name : 'document/annotation/text/app', },
+    { name : 'document/map/app' },
+    { name : 'document/settings/history' },
+    { name : 'document/settings/sharing' },
+    { name : 'my/upload/step1' },
+    { name : 'my/upload/step2' },
+    { name : 'my/upload/step3' },
+    { name : 'my/index' }
+ ]
 });
