0cd1fe733d6f569bb2916cbb658cb6f7e117595d
Rainer Simon <rainer.simon@ait.ac.at>
2016-06-25 10:41:16 +0200

    Minor edits (preparing for more refactoring)

--- app/assets/javascripts/document/annotation/text/editor/sections/place/minimap.js
+++ app/assets/javascripts/document/annotation/text/editor/sections/place/minimap.js
@@ -1,7 +1,43 @@
 define([], function(Formatting, PlaceUtils) {
 
-  var MiniMap = function() {
-  
+  var MiniMap = function(element) {
+    
+    var DEFAULT_ZOOM = 4,
+    
+        DEFAULT_CENTER = new L.LatLng(37.98, 23.73),
+    
+        CENTER_POINT = L.point(82, 70), // TODO derive from element dimensions
+    
+        awmcBase = L.tileLayer('http://a.tiles.mapbox.com/v3/isawnyu.map-knmctlkh/{z}/{x}/{y}.png', {
+          attribution: 'Tiles &copy; <a href="http://mapbox.com/" target="_blank">MapBox</a> | ' +
+                       'Data &copy; <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, CC-BY-SA | '+
+                       'Tiles and Data &copy; 2013 <a href="http://www.awmc.unc.edu" target="_blank">AWMC</a> ' +
+                       '<a href="http://creativecommons.org/licenses/by-nc/3.0/deed.en_US" target="_blank">CC-BY-NC 3.0</a>'
+        }),
+        
+        map = L.map(element[0], {
+          center: DEFAULT_CENTER,
+          zoom: DEFAULT_ZOOM,
+          zoomControl: false,
+          layers: [ awmcBase ]
+        }),
+        
+        markerLayer = L.layerGroup().addTo(map),
+        
+        setCenter = function(latLon) {
+          // Need to set as center first, then we can offset
+          map.invalidateSize();
+          map.setView(latLon, DEFAULT_ZOOM, { animate: false });
+          map.panTo(map.containerPointToLatLng(CENTER_POINT), { animate: false});
+        },
+        
+        setMarker = function(latLon) {
+          markerLayer.clearLayers();
+          L.marker(latLon).addTo(markerLayer);
+          setCenter(latLon);
+        };
+ 
+    this.setMarker = setMarker;
   };
 
   return MiniMap;
