5d27194fe4d424c9fb3ba1e0dba4058f3501cc8d
Rainer Simon <rainer.simon@ait.ac.at>
2016-07-18 09:41:18 +0200

    Minor edit

--- app/controllers/admin/users/UserAdminController.scala
+++ app/controllers/admin/users/UserAdminController.scala
@@ -10,22 +10,22 @@ import scala.concurrent.{ ExecutionContext, Future }
 import storage.DB
 
 class UserAdminController @Inject() (implicit val cache: CacheApi, val db: DB, ec: ExecutionContext, webjars: WebJarAssets) extends BaseAuthController {
-  
+
   def index = AsyncStack(AuthorityKey -> Admin) { implicit request =>
-    UserService.listUsers().map(users => 
+    UserService.listUsers(0, 500).map(users => 
       Ok(views.html.admin.users.index(users, webjars)))
   }
-  
+
   def showDetails(username: String) = AsyncStack(AuthorityKey -> Admin) { implicit request =>
     UserService.findByUsername(username).flatMap(_ match {
-      
+
       case Some(user) =>
         DocumentService.findByOwner(username).map(documents =>
           Ok(views.html.admin.users.details(user, documents)))
-      
+
       case None => Future.successful(NotFound)
-      
+
     })
   }
-  
+
 }
