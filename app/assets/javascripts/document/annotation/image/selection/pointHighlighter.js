c7d5691dd8074fba643f5382593fe4ccb8c428b9
Rainer Simon <rainer.simon@ait.ac.at>
2016-08-04 16:25:45 +0200

    Highlight-on-hover for point markers

--- app/assets/javascripts/document/annotation/image/selection/pointHighlighter.js
+++ app/assets/javascripts/document/annotation/image/selection/pointHighlighter.js
@@ -10,11 +10,23 @@ define([
             fill   : new ol.style.Fill({ color: '#4c4c4c', opacity: 0.6 }),
             stroke : new ol.style.Stroke({ color: '#262626', width: 1.5 })
           })
-        });
+        }),
+
+        POINT_STYLE_HI = new ol.style.Style({
+          image: new ol.style.Circle({
+            radius : 6,
+            fill   : new ol.style.Fill({ color: '#ff4c4c', opacity: 0.6 }),
+            stroke : new ol.style.Stroke({ color: '#262626', width: 1.5 })
+          })
+        }),
+
+        MIN_SELECTION_DISTANCE = 10;
 
     var PointHighlighter = function(olMap) {
 
       var pointVectorSource = new ol.source.Vector({}),
+
+          currentHighlight = false,
 
           selectPoint = function(coordinate) {
             var pointFeature = new ol.Feature({
@@ -22,6 +34,42 @@ define([
                 });
 
             pointVectorSource.addFeature(pointFeature);
+          },
+
+          /**
+           * Computes the distance (in pixel) between a screen (pixel) location, and
+           * a coordinate on the map.
+           */
+          computePxDistance = function(px, coord) {
+            var otherPx = olMap.getPixelFromCoordinate(coord),
+                dx = px[0] - otherPx[0],
+                dy = px[1] - otherPx[1];
+
+            return Math.sqrt(dx * dx + dy * dy);
+          },
+
+          onMousemove = function(e) {
+            if (!e.dragging) {
+              var closestFeature = pointVectorSource.getClosestFeatureToCoordinate(e.coordinate),
+                  closestPoint = (closestFeature) ? closestFeature.getGeometry().getClosestPoint(e.coordinate) : false;
+
+              if (closestPoint && computePxDistance(e.pixel, closestPoint) < MIN_SELECTION_DISTANCE) {
+                // Highlight the clostest feature, unless already highlighted
+                if (currentHighlight !== closestFeature) {
+
+                  // Un-highlight the previous highlight, if needed
+                  if (currentHighlight)
+                    currentHighlight.setStyle(POINT_STYLE);
+
+                  currentHighlight = closestFeature;
+                  closestFeature.setStyle(POINT_STYLE_HI);
+                }
+              } else if (currentHighlight) {
+                // Clear the previous highlight, if any
+                currentHighlight.setStyle(POINT_STYLE);
+                currentHighlight = false;
+              }
+            }
           },
 
           getAnnotationsAt = function() {
@@ -51,6 +99,8 @@ define([
         source: pointVectorSource,
         style: POINT_STYLE
       }));
+
+      olMap.on('pointermove', onMousemove);
 
       this.getAnnotationsAt = getAnnotationsAt;
       this.refreshAnnotation = refreshAnnotation;
