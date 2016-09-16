8c5d98acd1f897864e3fa62c705f05ddfaf59d71
Rainer Simon <rainer.simon@ait.ac.at>
2016-09-16 14:11:56 +0200

    Picking up date bounds from names + last edit timestamp (issue #160)

--- app/models/place/crosswalks/PleiadesCrosswalk.scala
+++ app/models/place/crosswalks/PleiadesCrosswalk.scala
@@ -1,30 +1,40 @@
 package models.place.crosswalks
 
 import com.vividsolutions.jts.geom.{ Coordinate, Geometry }
-import models.{ HasGeometry, HasNullableSeq }
+import models.{ HasDate, HasGeometry, HasNullableSeq }
 import models.place.{ CountryCode, Description, Gazetteer, GazetteerRecord, Name }
 import org.joda.time.DateTime
 import play.api.Logger
 import play.api.libs.json._
 import play.api.libs.json.Reads._
 import play.api.libs.functional.syntax._
+import models.place.TemporalBounds
 
 object PleiadesCrosswalk extends BaseGeoJSONCrosswalk {
   
   private val PLEIADES = Gazetteer("Pleiades")
+  
+  private def computeTemporalBounds(names: Seq[PleiadesName]): Option[TemporalBounds] = {
+    val startDate= names.flatMap(_.startDate)
+    val endDate = names.flatMap(_.endDate)
+    if (startDate.isEmpty || endDate.isEmpty)
+      None 
+    else
+      Some(TemporalBounds.fromYears(startDate.head, endDate.head))
+  }
 
   def fromJson(record: String): Option[GazetteerRecord] = super.fromJson[PleiadesRecord](record, { pleiades =>
     GazetteerRecord(
       pleiades.uri,
       PLEIADES,
       DateTime.now(),
-      None, // TODO lastChangedAt
+      pleiades.history.headOption.map(_.modified),
       pleiades.title,
       pleiades.description.map(d => Seq(new Description(d))).getOrElse(Seq.empty[Description]),
       pleiades.names.flatMap(_.toNames),
       pleiades.features.headOption.map(_.geometry), // TODO compute union?
       pleiades.representativePoint,
-      None, // TODO temporalBounds
+      computeTemporalBounds(pleiades.names), // TODO temporalBounds
       pleiades.placeTypes,
       None, // country code
       None, // population
@@ -35,7 +45,27 @@ object PleiadesCrosswalk extends BaseGeoJSONCrosswalk {
   
 }
 
-case class PleiadesName(attested: Option[String], romanized: Option[String], language: Option[String]) {
+case class HistoryRecord(modified: DateTime)
+
+object HistoryRecord extends HasDate {
+  
+  implicit val historyRecordReads: Reads[HistoryRecord] = (JsPath \ "modified").read[DateTime].map(HistoryRecord(_))
+  
+}
+
+case class PleiadesName(
+  attested: Option[String],
+  romanized: Option[String],
+  language: Option[String],
+  startDate: Option[Int],
+  endDate: Option[Int]
+) {
+  
+  // BAD bad Pleiades!
+  lazy val normalizedLanguage = language match {
+    case Some(language) if !language.trim.isEmpty => Some(language)
+    case _ => None
+  }
     
   lazy val toNames = Seq(attested, romanized).flatten.filter(!_.isEmpty).map(Name(_, language))
   
@@ -46,7 +76,9 @@ object PleiadesName {
    implicit val pleiadesNameReads: Reads[PleiadesName] = (
     (JsPath \ "attested").readNullable[String] and
     (JsPath \ "romanized").readNullable[String] and
-    (JsPath \ "language").readNullable[String]
+    (JsPath \ "language").readNullable[String] and
+    (JsPath \ "start").readNullable[Int] and
+    (JsPath \ "end").readNullable[Int]
   )(PleiadesName.apply _) 
 }
 
@@ -54,7 +86,7 @@ case class PleiadesRecord(
 
   uri: String,
 
-  // TODO lastChange
+  history: Seq[HistoryRecord],
   
   title: String,
   
@@ -78,6 +110,7 @@ object PleiadesRecord extends HasGeometry {
   
   implicit val pleiadesRecordReads: Reads[PleiadesRecord] = (
     (JsPath \ "uri").read[String] and
+    (JsPath \ "history").read[Seq[HistoryRecord]] and
     (JsPath \ "title").read[String] and
     (JsPath \ "description").readNullable[String] and
     (JsPath \ "names").readNullable[Seq[PleiadesName]].map(_.getOrElse(Seq.empty[PleiadesName])) and
