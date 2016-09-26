8e26a642bb763d23e1e97371a77cf38e880706ef
Rainer Simon <rainer.simon@ait.ac.at>
2016-09-26 15:20:51 +0200

    Removed temporary maintenance hack

--- app/controllers/admin/users/UserAdminController.scala
+++ app/controllers/admin/users/UserAdminController.scala
@@ -9,15 +9,9 @@ import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
 import controllers.WebJarAssets
 import play.api.Configuration
-import models.annotation.AnnotationService
-import scala.concurrent.Await
-import scala.concurrent.duration._
 
 class UserAdminController @Inject() (
     val config: Configuration,
-    
-    val annotations: AnnotationService,
-    
     val documents: DocumentService,
     val users: UserService,
     implicit val ctx: ExecutionContext,
@@ -28,28 +22,13 @@ class UserAdminController @Inject() (
     users.listUsers(0, 500).map(users => 
       Ok(views.html.admin.users.index(users)))
   }
-  
-  private def regenerateLinks(username: String) = {
-    documents.findByOwner(username, false, 0, Int.MaxValue).map { page =>
-      val docs = page.items
-      docs.map { doc =>
-        annotations.findByDocId(doc.getId).map { a =>
-          a.foreach { annotation =>
-            Await.result(annotations.insertOrUpdateGeoTagsForAnnotation(annotation._1), 1.minute)
-          }
-        }
-      }
-    }
-  }
 
   def showDetails(username: String) = AsyncStack(AuthorityKey -> Admin) { implicit request =>
     users.findByUsername(username).flatMap(_ match {
 
       case Some(user) =>
-        documents.findByOwner(username).map { documents => 
-          regenerateLinks(user.user.getUsername)
-          Ok(views.html.admin.users.details(user, documents)) 
-        }
+        documents.findByOwner(username).map(documents =>
+          Ok(views.html.admin.users.details(user, documents))) 
 
       case None => Future.successful(NotFoundPage)
 
