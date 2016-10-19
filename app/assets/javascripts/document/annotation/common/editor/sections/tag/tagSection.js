3313cee45650e81ab76342567aa4cc38d3060ed8
Rainer Simon <rainer.simon@ait.ac.at>
2016-10-19 09:47:42 +0200

    Tagging: first functional version

--- app/assets/javascripts/document/annotation/common/editor/sections/tag/tagSection.js
+++ app/assets/javascripts/document/annotation/common/editor/sections/tag/tagSection.js
@@ -3,6 +3,10 @@ define([
   'document/annotation/common/editor/sections/section'
 ], function(Config, Section) {
 
+  var DELETE_WIDTH = 23,
+
+      ANIM_DURATION = 150;
+
   var TagSection = function(parent, annotation) {
     var element = jQuery(
           '<div class="section tags">' +
@@ -15,47 +19,121 @@ define([
 
         queuedUpdates = [],
 
+        /**
+         * Creates a new tag element and attaches the tag to it as data.
+         *
+         * Takes either a tag object or a string as input.
+         */
+        createTag = function(charsOrTag) {
+          var tag = (charsOrTag.type) ? charsOrTag :
+                { type: 'TAG', last_modified_by: Config.me, value: charsOrTag.trim() },
+
+              li = jQuery('<li><span class="label">' + tag.value + '</span>' +
+                '<span class="delete"><span class="icon">&#xf014;</span></span></li>');
+
+          li.data('tag', tag);
+          return li;
+        },
+
+        /** Initializes the tag list from the annotation bodies **/
         init = function() {
           jQuery.each(annotation.bodies, function(idx, body) {
             if (body.type === 'TAG')
-              addTag(body.value);
+              taglist.append(createTag(body));
           });
         },
 
-        hasChanged = function() {
-          return queuedUpdates.length > 0;
+        /** Tests if the given character string exists as a tag already **/
+        exists = function(chars) {
+          var existing = jQuery.grep(taglist.children(), function(el) {
+            var tagChars = jQuery(el).find('.label').text();
+            return tagChars === chars;
+          });
+          return existing.length > 0;
         },
 
-        commit = function() {
-          jQuery.each(queuedUpdates, function(idx, fn) { fn(); });
-        },
-
-        destroy = function() {
-          element.remove();
-        },
-
+        /** Adds a new tag to the annotation **/
         addTag = function(chars) {
+          if (!exists(chars)) {
+            var li = createTag(chars);
+            taglist.append(li);
+            queuedUpdates.push(function() { annotation.bodies.push(li.data('tag')); });
+          }
+        },
+
+        /** Deletes a tag from the annotation **/
+        deleteTag = function(li) {
+          var tag = li.data('tag');
+
+          li.remove();
           queuedUpdates.push(function() {
-            annotation.bodies.push({
-              type: 'TAG', last_modified_by: Config.me, value: chars
-            });
+            var idx = annotation.bodies.indexOf(tag);
+            if (idx > -1)
+              annotation.bodies.splice(idx, 1);
           });
-
-          taglist.append('<li>' + chars + '</li>');
         },
 
+        /** Shows the delete button on the given tag element **/
+        showDeleteButton = function(li) {
+          var delIcon = li.find('.delete');
+          li.animate({ 'padding-right' : DELETE_WIDTH }, ANIM_DURATION);
+          delIcon.animate({ 'width': DELETE_WIDTH }, ANIM_DURATION);
+        },
+
+        /** Hides all currently visible delete buttons **/
+        hideAllDeleteButtons = function() {
+          jQuery.each(taglist.find('li'), function(idx, el) {
+            var li = jQuery(el),
+                delIcon = li.find('.delete');
+                isClicked = delIcon.width() > 0;
+
+            if (isClicked) {
+              li.animate({ 'padding-right' : 0 }, ANIM_DURATION);
+              delIcon.animate({ 'width': 0 }, ANIM_DURATION);
+            }
+          });
+        },
+
+        /** Click toggles the delete button or deletes, depending on state & click target **/
         onTagClicked = function(e) {
-          var chars = jQuery(e.target).text();
-          console.log(chars);
+          var isDelete = (e.target).closest('.delete'),
+              li = jQuery(e.target).closest('li'),
+              chars = li.find('.label').text();
+
+          if (isDelete) {
+            deleteTag(li);
+          } else {
+            showDeleteButton(li);
+            hideAllDeleteButtons();
+          }
         },
 
+        /** Text entry field: new tags are created on ENTER **/
         onKeyDown = function(e) {
           if (e.keyCode === 13) {
-            addTag(textarea.text().trim());
+            var tags = textarea.text().split(',');
+            jQuery.each(tags, function(idx, chars) {
+              addTag(chars.trim());
+            });
             textarea.empty();
             textarea.blur();
             return false;
           }
+        },
+
+        /** @override **/
+        hasChanged = function() {
+          return queuedUpdates.length > 0;
+        },
+
+        /** @override **/
+        commit = function() {
+          jQuery.each(queuedUpdates, function(idx, fn) { fn(); });
+        },
+
+        /** @override **/
+        destroy = function() {
+          element.remove();
         };
 
     init();
