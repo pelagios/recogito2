fcc566b58312f8b0a1ae57a3c3f60af38a7e8e70
Rainer Simon <rainer.simon@ait.ac.at>
2016-10-27 14:40:39 +0200

    Dashboard: top users

--- app/assets/javascripts/admin/dashboard.js
+++ app/assets/javascripts/admin/dashboard.js
@@ -11,20 +11,50 @@ require(['common/ui/formatting'], function(Formatting) {
         totalAnnotations = jQuery('.total-annotations .number'),
         registeredUsers = jQuery('.registered-users .number'),
 
+        topUsers = jQuery('.top-users table'),
+
+        refreshHighscores = function(scores) {
+
+          var maxScore = Math.max(jQuery.map(scores, function(score) {
+                return score.value;
+              })),
+
+              toPercent = function(score) {
+                return score / maxScore * 100;
+              },
+
+              createRow = function(username, count) {
+                return jQuery(
+                  '<tr>' +
+                    '<td><a href=/admin/users/' + username + '">' + username + '</a></td>' +
+                    '<td>' +
+                      '<div class="meter">' +
+                        '<div class="bar" style="width:' + toPercent(count) + '%"></div>' +
+                      '</div>' +
+                    '</td>' +
+                    '<td>' + Formatting.formatNumber(count) + ' Edits</td>' +
+                  '</tr>');
+              };
+
+          jQuery.each(scores.reverse(), function(idx, score) {
+            topUsers.append(createRow(score.username, score.value));
+          });
+        },
+
         refreshContributionStats = function() {
           var fillNumber = function(field, num) {
                 field.html(Formatting.formatNumber(num));
               };
 
           jsRoutes.controllers.api.StatsAPIController.getDashboardStats().ajax().done(function(stats) {
-
-            console.log(stats);
-
             fillNumber(totalEdits, stats.contributions.total_contributions);
             fillNumber(totalAnnotations, stats.total_annotations);
             fillNumber(registeredUsers, stats.total_users);
+
+            refreshHighscores(stats.contributions.by_user);
           });
         },
+
 
         refresh = function() {
           refreshContributionStats();
