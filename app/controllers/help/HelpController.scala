0fd8c069c3f01294a0c77c5a39eb33283ad96dac
Rainer Simon <rainer.simon@ait.ac.at>
2016-11-30 13:00:53 +0100

    Refactoring

--- app/controllers/help/HelpController.scala
+++ app/controllers/help/HelpController.scala
@@ -4,13 +4,15 @@ import play.api.mvc.Controller
 import play.api.mvc.Action
 
 class HelpController extends Controller {
-  
-  def index     = Action { Redirect(routes.HelpController.showTour()) }
-  
-  def showAbout = Action { implicit request => Ok(views.html.help.about()) }
-  
-  def showFAQ   = Action { implicit request => Ok(views.html.help.faq())   }
-    
-  def showTour  = Action { implicit request => Ok(views.html.help.tour()) }
-  
+
+  // TODO track visits
+
+  def index         = Action { Redirect(routes.HelpController.showTutorial()) }
+
+  def showAbout     = Action { implicit request => Ok(views.html.help.about()) }
+
+  def showFAQ       = Action { implicit request => Ok(views.html.help.faq())   }
+
+  def showTutorial  = Action { implicit request => Ok(views.html.help.tutorial()) }
+
 }
