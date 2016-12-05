343638190558e1c78748bef5ed0fe36b175e36f9
Rainer Simon <rainer.simon@ait.ac.at>
2016-12-05 08:47:42 +0100

    Annotation rendering error handling

--- app/assets/javascripts/document/annotation/text/selection/highlighter.js
+++ app/assets/javascripts/document/annotation/text/selection/highlighter.js
@@ -192,18 +192,20 @@ define([
                 range = rangy.createRange(),
                 positions, spans;
 
-            console.log(annotation);
-
             if (previousBounds && intersects(previousBounds, bounds)) {
               positions = charOffsetsToDOMPosition([ bounds.start, bounds.end ]);
               range.setStart(positions[0].node, positions[0].offset);
               range.setEnd(positions[1].node, positions[1].offset);
               spans = wrapRange(range);
-            } else {
+            } else try {
               // Fast rendering through Rangy's API
               setNonOverlappingRange(range, anchor, quote.length);
               classApplier.applyToRange(range);
               spans = [ range.getNodes()[0].parentElement ];
+            } catch (e) {
+              console.log('Error rendering annotation');
+              console.log(e);
+              console.log(annotation);
             }
 
             // Attach annotation data as payload to the SPANs and set id, if any
