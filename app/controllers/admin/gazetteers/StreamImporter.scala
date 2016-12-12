0cff7a03f0538af3aa18ed2d02048d0cac9726ad
Rainer Simon <rainer.simon@ait.ac.at>
2016-12-12 16:46:42 +0100

    Minor edits

--- app/controllers/admin/gazetteers/StreamImporter.scala
+++ app/controllers/admin/gazetteers/StreamImporter.scala
@@ -25,19 +25,14 @@ class StreamImporter(implicit materializer: Materializer) {
     val source = StreamConverters.fromInputStream(() => is, 1024)
       .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = Int.MaxValue, allowTruncation = false))
       .map(_.utf8String)
-      .withAttributes(ActorAttributes.supervisionStrategy(decider))
       
     val parser = Flow.fromFunction[String, Option[GazetteerRecord]](crosswalk)
       .withAttributes(ActorAttributes.supervisionStrategy(decider))
       .grouped(BATCH_SIZE)
-    
+      
     val importer = Sink.foreach[Seq[Option[GazetteerRecord]]] { records =>
       val toImport = records.flatten
-      try {
-        Await.result(places.importRecords(toImport), 120.minute)
-      } catch { case t: Throwable =>
-        t.printStackTrace
-      }
+      Await.result(places.importRecords(toImport), 60.minutes)
     }
     
     val graph = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder =>
@@ -47,9 +42,9 @@ class StreamImporter(implicit materializer: Materializer) {
       source ~> parser ~> importer
       
       ClosedShape
-    })
-    
-    graph.run()    
+    }).withAttributes(ActorAttributes.supervisionStrategy(decider))
+        
+    graph.run()
   }
   
 }
