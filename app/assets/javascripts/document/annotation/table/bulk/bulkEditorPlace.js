2de98298c4008ac5ea93b4e2a742184ad206358e
Rainer Simon <rainer.simon@ait.ac.at>
2017-01-16 16:09:45 +0100

    Table automatching: wizard instructions and styling

--- app/assets/javascripts/document/annotation/table/bulk/bulkEditorPlace.js
+++ app/assets/javascripts/document/annotation/table/bulk/bulkEditorPlace.js
@@ -14,23 +14,58 @@ define([
             return '<option value="' + idx + '">' + name + '</option>';
           }).join(''),
 
+        gazetteers = // TODO
+          '<option></option>',
+
         form = jQuery(
           '<form class="crud">' +
-            // '<p class="caption"></p>' +
+            '<p class="instructions">' +
+              'To generate automatic gazetteer matches for your data, select a column below. ' +
+              'The contents of this column will be matched against the built-in gazetteers.' +
+            '</p>' +
+
             '<dl id="place-column">' +
-              '<dt><label for="place-column">Place name column</label></dt>' +
+              '<dt><label for="place-column">Placename</label></dt>' +
               '<dd><select>' + options + '</select></dd>' +
+              '<dd class="info">Required</dd>' +
             '</dl>' +
 
+            '<p class="instructions">' +
+              'If your data includes geo-coordinates, you can use them to ' +
+              'disambiguate the matches.' +
+            '</p>' +
+
             '<dl id="lat-column">' +
-              '<dt><label for="lat-column">Latitude column</label></dt>' +
+              '<dt><label for="lat-column">Latitude</label></dt>' +
               '<dd><select>' + options + '</select></dd>' +
+              '<dd class="info">Optional</dd>' +
             '</dl>' +
 
             '<dl id="lon-column">' +
-              '<dt><label for="lon-column">Longitude column</label></dt>' +
+              '<dt><label for="lon-column">Longitude</label></dt>' +
               '<dd><select>' + options + '</select></dd>' +
+              '<dd class="info">Optional</dd>' +
             '</dl>' +
+
+            '<p class="instructions">' +
+              'Optionally, choose a specific gazetteer to be preferred, ' +
+              'or used exclusively.' +
+            '</p>' +
+
+            '<dl id="preferred-gazetteer">' +
+              '<dt><label for="preferred-gazetteer">Preferred gazetteer</label></dt>' +
+              '<dd><select disabled="true">' + gazetteers + '</select></dd>' +
+              '<dd class="info">Optional</dd>' +
+            '</dl>' +
+
+            '<dl id="use-exclusive">' +
+              '<dt></dt>' +
+              '<dd>' +
+                '<input type="checkbox" id="exclusive" disabled="true">' +
+                '<label for="exclusive">Exclusive</label>' +
+              '</dd>' +
+            '</dl>' +
+
             '<div class="buttons">' +
               '<button type="submit" class="btn">Go</button>' +
               '<button class="btn outline cancel">Cancel</button>' +
@@ -93,7 +128,7 @@ define([
     form.submit(onSubmit);
     btnCancel.click(onCancel);
 
-    Modal.apply(this, [ 'Place Annotation', form, 'wizard' ]);
+    Modal.apply(this, [ 'Automatic Gazetteer Resolution', form, 'wizard' ]);
 
     self.open();
   };
