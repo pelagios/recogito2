e4602cc953a7e8beed2a4de8f0d05f38d3b453d2
Rainer Simon <rainer.simon@ait.ac.at>
2017-01-19 16:10:34 +0100

    Added settings dialog placeholder

--- app/assets/javascripts/document/downloads/app.js
+++ app/assets/javascripts/document/downloads/app.js
@@ -3,11 +3,31 @@ require.config({
   fileExclusionRegExp: /^lib$/
 });
 
-require([], function() {
+require([
+  'common/ui/modal'
+], function(Modal) {
+
+  var SettingsModal = function() {
+    var self = this,
+
+        body = jQuery('<div></div>');
+
+    Modal.apply(this, [ 'Settings', body, 'settings' ]);
+
+    self.open();
+  };
+  SettingsModal.prototype = Object.create(Modal.prototype);
 
   jQuery(document).ready(function() {
 
-    // TODO
+    var btnSettings = jQuery('.settings'),
+
+        openSettings = function() {
+          new SettingsModal();
+          return false;
+        };
+
+    btnSettings.click(openSettings);
 
   });
 
