f46b07b5a674b5171b4b13049eb9871752b7a08b
Rainer Simon <rainer.simon@ait.ac.at>
2017-10-18 15:15:22 +0200

    Cleanup

--- app/models/BaseService.scala
+++ app/models/BaseService.scala
@@ -47,16 +47,6 @@ trait BaseService {
       .map(r => (r.into(t), r.into(v)))
       .groupBy(_._1)
       .mapValues(_.map(_._2).filter(record => isNotNull(record)))
-      
-  protected def groupUnionJoinResult[T <: Record, V <: Record, U <: Record](records: Seq[Record], t: Class[T], v: Class[V], u: Class[U]): Map[T, (Seq[V], Seq[U])] = {
-    val groupedV = groupLeftJoinResult(records, t, v)
-    val groupedU = groupLeftJoinResult(records, t, u)
-    
-    val keys = (groupedV.keySet ++ groupedU.keySet).toSeq
-    keys.map { t =>
-      t -> (groupedV.get(t).getOrElse(Seq.empty[V]), groupedU.get(t).getOrElse(Seq.empty[U]))
-    }.toMap    
-  }
 
   /** Boilerplate code for conducting a cache lookup, followed by DB lookup if nothing in cache **/
   protected def cachedLookup[T: ClassTag](prefix: String, key: String, 
