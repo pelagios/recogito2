0fc2211f36bae7e7a110a08650ffebe1615d3605
Rainer Simon <rainer.simon@ait.ac.at>
2018-01-23 11:28:26 +0100

    Starting annotation migration util

---
+++ app/storage/migration/legacy/LegacyAnnotation.scala
@@ -0,0 +1,47 @@
+package storage.migration.legacy
+
+import java.util.UUID
+import services.{ContentType, HasContentTypeList, HasDate}
+import services.annotation.{Annotation, AnnotatedObject}
+import org.joda.time.DateTime
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
+case class LegacyAnnotation(
+  annotationId: UUID,
+  versionId: UUID,
+  annotates: AnnotatedObject,
+  contributors: Seq[String],
+  anchor: String,
+  lastModifiedBy: Option[String],
+  lastModifiedAt: DateTime,
+  bodies: Seq[LegacyAnnotationBody]
+) {
+  
+  def toNewAPI = Annotation(
+    annotationId,
+    versionId,
+    annotates,
+    contributors,
+    anchor,
+    lastModifiedBy,
+    lastModifiedAt,
+    bodies.map(_.toNewAPI)
+  )
+  
+}
+
+object LegacyAnnotation extends HasDate {
+
+  implicit val legacyAnnotationFormat: Format[LegacyAnnotation] = (
+    (JsPath \ "annotation_id").format[UUID] and
+    (JsPath \ "version_id").format[UUID] and
+    (JsPath \ "annotates").format[AnnotatedObject] and
+    (JsPath \ "contributors").format[Seq[String]] and
+    (JsPath \ "anchor").format[String] and
+    (JsPath \ "last_modified_by").formatNullable[String] and
+    (JsPath \ "last_modified_at").format[DateTime] and
+    (JsPath \ "bodies").format[Seq[LegacyAnnotationBody]]
+  )(LegacyAnnotation.apply, unlift(LegacyAnnotation.unapply))
+
+}
---
+++ app/storage/migration/legacy/LegacyAnnotationBody.scala
@@ -0,0 +1,44 @@
+package storage.migration.legacy
+
+import services.HasDate
+import services.annotation.{AnnotationBody, AnnotationStatus, Reference}
+import org.joda.time.DateTime
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.api.libs.functional.syntax._
+
+case class LegacyAnnotationBody(
+  hasType: AnnotationBody.Type,
+  lastModifiedBy: Option[String],
+  lastModifiedAt: DateTime,
+  value: Option[String],
+  uri: Option[String],
+  note: Option[String],
+  status: Option[AnnotationStatus]
+) {
+  
+  def toNewAPI = AnnotationBody(
+    hasType,
+    lastModifiedBy,
+    lastModifiedAt,
+    value,
+    uri.map(Reference(_)),
+    note,
+    status
+  )
+  
+}
+
+object LegacyAnnotationBody extends Enumeration with HasDate {
+
+  implicit val legacyAnnotationBodyFormat: Format[LegacyAnnotationBody] = (
+    (JsPath \ "type").format[AnnotationBody.Value] and
+    (JsPath \ "last_modified_by").formatNullable[String] and
+    (JsPath \ "last_modified_at").format[DateTime] and
+    (JsPath \ "value").formatNullable[String] and
+    (JsPath \ "uri").formatNullable[String] and
+    (JsPath \ "note").formatNullable[String] and
+    (JsPath \ "status").formatNullable[AnnotationStatus]
+  )(LegacyAnnotationBody.apply, unlift(LegacyAnnotationBody.unapply))
+
+}
