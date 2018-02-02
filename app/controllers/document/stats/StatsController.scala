f8fe2a4789bbe2222f5d09c39e726418995551aa
Rainer Simon <rainer.simon@ait.ac.at>
2018-02-02 16:32:02 +0100

    Annotation stats: schema change, service skeleton, tag stats method

--- app/controllers/document/stats/StatsController.scala
+++ app/controllers/document/stats/StatsController.scala
@@ -1,8 +1,9 @@
 package controllers.document.stats
 
 import com.mohiva.play.silhouette.api.Silhouette
-import controllers.{BaseOptAuthController, Security, HasVisitLogging}
+import controllers.{BaseOptAuthController, Security, HasVisitLogging, HasPrettyPrintJSON}
 import javax.inject.{Inject, Singleton}
+import services.annotation.stats.AnnotationStatsService
 import services.document.DocumentService
 import services.user.UserService
 import services.user.Roles._
@@ -10,6 +11,8 @@ import services.visit.VisitService
 import org.webjars.play.WebJarsUtil
 import play.api.Configuration
 import play.api.mvc.ControllerComponents
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
@@ -17,20 +20,23 @@ class StatsController @Inject() (
   val components: ControllerComponents,
   val config: Configuration,
   val documents: DocumentService,
+  val statsService: AnnotationStatsService,
   val users: UserService,
   val silhouette: Silhouette[Security.Env],
   implicit val visitService: VisitService,
   implicit val webjars: WebJarsUtil,
   implicit val ctx: ExecutionContext
-) extends BaseOptAuthController(components, config, documents, users) with HasVisitLogging {
-
-
+) extends BaseOptAuthController(components, config, documents, users) with HasVisitLogging with HasPrettyPrintJSON {
+  
+  implicit val tuple2Writes: Writes[Tuple2[String, Long]] = (
+    (JsPath \ "value").write[String] and
+    (JsPath \ "count").write[Long]
+  )(t => (t._1, t._2))
+  
   /** TODO this view should be available without login, if the document is set to public **/
   def showDocumentStats(documentId: String, tab: Option[String]) = silhouette.UserAwareAction.async { implicit request =>
     documentReadResponse(documentId, request.identity,  { case (doc, accesslevel) =>
-      
-      logDocumentView(doc.document, None, accesslevel)
-      
+      logDocumentView(doc.document, None, accesslevel)      
       tab.map(_.toLowerCase) match {
         case Some(t) if t == "activity" =>
           Future.successful(Ok(views.html.document.stats.activity(doc, request.identity, accesslevel)))
@@ -45,6 +51,12 @@ class StatsController @Inject() (
           Future.successful(Ok(views.html.document.stats.activity(doc, request.identity, accesslevel)))
       }
     })
+  }
+  
+  def getTagStats(documentId: String) = silhouette.UserAwareAction.async { implicit request =>
+    statsService.getTagStats(documentId).map { buckets =>
+      jsonOk(Json.toJson(buckets))
+    }
   }
 
 }
---
+++ app/services/annotation/stats/AnnotationStatsService.scala
@@ -0,0 +1,38 @@
+package services.annotation.stats
+
+import collection.JavaConverters._
+import com.sksamuel.elastic4s.ElasticDsl._
+import javax.inject.{Inject, Singleton}
+import org.elasticsearch.search.aggregations.bucket.nested.InternalNested
+import scala.concurrent.ExecutionContext
+import services.annotation.HasAnnotationIndexing
+import storage.es.ES
+import services.annotation.AnnotationBody
+import org.elasticsearch.search.aggregations.bucket.filter.InternalFilter
+import org.elasticsearch.search.aggregations.bucket.terms.StringTerms
+
+@Singleton
+class AnnotationStatsService @Inject() (
+  implicit val ctx: ExecutionContext,
+  implicit val es: ES
+) extends HasAnnotationIndexing {
+  
+  def getTagStats(documentId: String) =
+    es.client execute {
+      search(ES.RECOGITO / ES.ANNOTATION) query {
+        termQuery("annotates.document_id" -> documentId)
+      } size 0 aggregations (
+        nestedAggregation("per_body", "bodies") subaggs (
+          filterAggregation("tags_only").query(termQuery("bodies.type" -> AnnotationBody.TAG.toString)) subaggs (
+            termsAggregation("by_tag") field ("bodies.value.raw")
+          )
+        )
+      ) 
+    } map { _.aggregations
+      .getAs[InternalNested]("per_body").getAggregations
+      .get[InternalFilter]("tags_only").getAggregations
+      .get[StringTerms]("by_tag").getBuckets.asScala
+      .map(b => (b.getKeyAsString, b.getDocCount))
+    }
+  
+}
