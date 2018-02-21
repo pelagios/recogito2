9bcd2f9a508adae7a01bfcbddcfdf6b7762ed659
Rainer Simon <rainer.simon@ait.ac.at>
2018-02-21 17:03:24 +0100

    Added a few labels from the #EnableBabel workshop

---
+++ conf/messages.el
@@ -0,0 +1,42 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Splash page
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+landing.index.page_title=Καλωσήλθατε στο Recogito
+landing.index.link.tutorial=Οδηγος χρήσης
+landing.index.link.login=Σύνδεση
+
+# Jumbotron and registration
+landing.index.jumbotron.tagline.1=Επισημείωση διασυνδεδεμένων δεδομένων
+landing.index.jumbotron.tagline.2=χωρίς τα άγκυστρα
+landing.index.jumbotron.blurb=Δημιουργήστε χάρτες από κείμενα. Επισημειώστε εικόνες. Συνδέστε την έρευνά σας με άλλα δεδομένα στο διαδίκτυο, βελτιώστε την ορατότητα και προωθηστε την επανάχρησή της. Χωρίς να χρειάζεται εξειδίκευση στο σημασιολογικό ιστό.
+landing.index.jumbotron.label.learn_more=Learn More
+landing.index.registration_form.placeholder.username=Επιλέξετε όνομα χρήστη
+landing.index.registration_form.placeholder.email=Διεύθυνση ηλεκτρονικού ταχυδρομείου
+landing.index.registration_form.placeholder.password=Δημιουργήστε κωδικό ασφαλείας
+landing.index.registration_form.placeholder.register=Εγγραφείτε στο Recogito
+landing.index.registration_form.disclaimer.text=Agree to our <a href="{0}">Terms and Conditions of Use</a> \
+  and the storage of your E-Mail address.
+
+# News
+landing.index.news.title=New in Recogito:
+landing.index.news.subtitle=China Historical GIS
+landing.index.news.html=We are excited to announce that data from the \
+  <a href="http://maps.cga.harvard.edu/chgis/" target="_blank">China Historical GIS</a> is now \
+  available in Recogito. This adds more than 71,000 places of Dynastic China from 221 BCE to 1911 \
+  CE to our geographical index. Our thanks to Lex Berman and the \
+  <a href="http://gis.harvard.edu/" target="_blank">Harvard Center for Geographic Analysis</a> for \
+  maintaining this brilliant resource!
+
+# Key messages
+landing.index.tiles.1.title=Shared Collections
+landing.index.tiles.1.text=Upload texts and images. Build your own collections. Invite others \
+  to collaborate.
+landing.index.tiles.2.title=Collaborative Annotation
+landing.index.tiles.2.text=Create notes. Tag. Discuss. Recogito tracks provenance and maintains \
+  version history.
+landing.index.tiles.3.title=Semantics Included
+landing.index.tiles.3.text=Tag places and persons with controlled vocabularies easily with \
+  built-in support features.
+landing.index.tiles.4.title=Open Data
+landing.index.tiles.4.text=Show the results of your work on the Web. Publish your data as Linked \
+  Open Data.
---
+++ conf/messages.nl
@@ -0,0 +1,37 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Splash page
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+landing.index.page_title=Welkom bij Recogito
+landing.index.link.tutorial=Tutorial
+landing.index.link.login=Inloggen
+
+# Jumbotron and registration
+landing.index.jumbotron.tagline.1=Linked Data annotatie
+landing.index.jumbotron.tagline.2=zonder de accolades
+landing.index.jumbotron.blurb=Creeër landkaarten van tekst. Annoteer afbeeldingen. Verbind uw onderzoek met andere databronnen op het Web en maak het zichtbaarder en herbruikbaar. Semantic Web expertise is niet nodig.
+landing.index.jumbotron.label.learn_more=Meer weten
+landing.index.registration_form.placeholder.username=Kies een gebruikersnaam
+landing.index.registration_form.placeholder.email=Emailadres
+landing.index.registration_form.placeholder.password=Kies een wachtwoord
+landing.index.registration_form.placeholder.register=Register voor Recogito
+landing.index.registration_form.disclaimer.text=Agree to our <a href="{0}">Terms and Conditions of Use</a> \
+  and the storage of your E-Mail address.
+
+# News
+landing.index.news.title=Nieuw in Recogito:
+landing.index.news.subtitle=China Historical GIS
+landing.index.news.html=De data van <a href="http://maps.cga.harvard.edu/chgis/" target="_blank">China Historical GIS</a> is nu beschikbaar in Recogito. Hiermee zijn meer dan 71.000 plaatsnamen uit het Chinese Keizerrijk van 221 v. Chr. tot 1911 aan onze geografische index toegevoegd. Met dank aan Lex Berman en het <a href="http://gis.harvard.edu/" target="_blank">Harvard Center for Geographic Analysis</a> voor het onderhouden van deze resource! 
+
+# Key messages
+landing.index.tiles.1.title=Shared Collections
+landing.index.tiles.1.text=Upload texts and images. Build your own collections. Invite others \
+  to collaborate.
+landing.index.tiles.2.title=Collaborative Annotation
+landing.index.tiles.2.text=Create notes. Tag. Discuss. Recogito tracks provenance and maintains \
+  version history.
+landing.index.tiles.3.title=Semantics Included
+landing.index.tiles.3.text=Tag places and persons with controlled vocabularies easily with \
+  built-in support features.
+landing.index.tiles.4.title=Open Data
+landing.index.tiles.4.text=Show the results of your work on the Web. Publish your data as Linked \
+  Open Data.
