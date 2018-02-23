a51fc6b22ebfc611c5b400310fe3276879ca5033
Rainer Simon <rainer.simon@ait.ac.at>
2018-02-23 09:08:28 +0100

    French + Dutch labels for editor popup

---
+++ app/assets/javascripts/common/i18n/document/annotation/nls/nl/editor.js
@@ -0,0 +1,15 @@
+define({
+  'label_place': 'Plaats',
+  'label_person': 'Persoon',
+  'label_event': 'Gebeurtenis',
+
+  'placeholder_add_comment': 'Commentaar toevoegen...',
+  'placeholder_add_tag': 'Tag toevoegen...',
+  'placeholder_transcribe': 'Transcribeer...',
+
+  'dropdown_edit': 'Aanpassen',
+  'dropdown_delete': 'Annuleren',
+
+  'btn_cancel': 'Annuleren',
+  'btn_ok_and_next': 'OK &amp; Volgende'
+});
