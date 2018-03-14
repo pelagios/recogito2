15062d350c87dfda036c9d1b3516ad7fa1b7da83
Rainer Simon <rainer.simon@ait.ac.at>
2018-03-14 14:48:48 +0100

    Additional Farsi labels

---
+++ app/assets/javascripts/common/i18n/document/annotation/nls/fa/editor.js
@@ -0,0 +1,15 @@
+define({
+  'label_place': 'مکان',
+  'label_person': 'فرد',
+  'label_event': 'رویداد',
+
+  'placeholder_add_comment': 'نظر بدهید ...',
+  'placeholder_add_tag': 'برچسب اضافه کنید ...',
+  'placeholder_transcribe': 'رونویسی ...',
+
+  'dropdown_edit': 'ویرایش',
+  'dropdown_delete': 'پاک کردن',
+
+  'btn_cancel': 'لغو',
+  'btn_ok_and_next': 'OK! بعدی'
+});
