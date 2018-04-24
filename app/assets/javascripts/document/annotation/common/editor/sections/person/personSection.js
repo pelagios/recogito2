f30e1a56332eb5c87359962c776836e0b4acda80
Rainer Simon <rainer.simon@ait.ac.at>
2018-04-24 09:19:03 +0200

    UI fix: NER-generated person tags are now deletable

--- app/assets/javascripts/document/annotation/common/editor/sections/person/personSection.js
+++ app/assets/javascripts/document/annotation/common/editor/sections/person/personSection.js
@@ -8,28 +8,24 @@ define([
     var self = this,
 
         element = (function() {
-          var lastModified = jQuery(
-                '<div class="last-modified">' +
-                  '<a class="by" href="/' + personBody.last_modified_by + '">' +
-                    personBody.last_modified_by + '</a>' +
-                  '<span class="at">' +
-                    Formatting.timeSince(personBody.last_modified_at) +
-                  '</span>' +
-                '</div>'),
-
-              el = jQuery(
+          var el = jQuery(
                 '<div class="section category person">' +
                   '<div class="category-icon">&#xe863;</div>' +
                   '<div class="info"><span>Marked as a Person</span></div>' +
                 '</div>');
 
-          if (personBody.last_modified_by) {
-            var infoEl = el.find('.info');
-            infoEl.append(lastModified);
+          if (personBody.last_modified_by)
+            el.find('.info').append(jQuery(
+              '<div class="last-modified">' +
+                '<a class="by" href="/' + personBody.last_modified_by + '">' +
+                  personBody.last_modified_by + '</a>' +
+                '<span class="at">' +
+                  Formatting.timeSince(personBody.last_modified_at) +
+                '</span>' +
+              '</div>'));
 
-            if (Config.writeAccess)
-              infoEl.append('<button class="btn tiny delete icon">&#xf014;</button>');
-          }
+          if (Config.writeAccess)
+            el.find('.info').append('<button class="btn tiny delete icon">&#xf014;</button>');
 
           parent.append(el);
           return el;
