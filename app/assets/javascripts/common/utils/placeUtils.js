6b4862627d96fc99bf4ba923d411e244d7bb9b49
Rainer Simon <rainer.simon@ait.ac.at>
2018-04-26 14:41:11 +0200

    Fix to display ugly MoEML URLs nicely

--- app/assets/javascripts/common/utils/placeUtils.js
+++ app/assets/javascripts/common/utils/placeUtils.js
@@ -1,13 +1,15 @@
 define(['common/ui/countries'], function(Countries) {
 
   // TODO fetch this information from the server, so we can feed it from the DB
-  var KNOWN_GAZETTEERS = [
+  var TOKEN = '{{id}}',
+
+      KNOWN_GAZETTEERS = [
         { shortcode: 'chgis',    url_patterns: [ 'http://maps.cga.harvard.edu/tgaz/placename/hvd_' ], color: '#9467bd' },
         { shortcode: 'dare',     url_patterns: [ 'http://dare.ht.lu.se/places/' ], color: '#ff7f0e' },
-        { shortcode: 'dpp',      url_patterns: [ 'http://dpp.oeaw.ac.at/places/dpp_places.xls#' ], color: '#efca10' },
+        { shortcode: 'dpp',      url_patterns: [ 'http://dpp.oeaw.ac.at/places/dpp_places.xls#}' ], color: '#efca10' },
         { shortcode: 'geonames', url_patterns: [ 'http://sws.geonames.org/'], color: '#2ca02c' },
         { shortcode: 'pleiades', url_patterns: [ 'http://pleiades.stoa.org/places/' ], color: '#1f77b4' },
-        { shortcode: 'moeml',    url_patterns: [ 'http://mapoflondon.uvic.ca/' ], color: '#8c564b' }
+        { shortcode: 'moeml',    url_patterns: [ 'http://mapoflondon.uvic.ca/{{id}}.htm' ], color: '#8c564b' }
       ],
 
       /**
@@ -201,14 +203,31 @@ define(['common/ui/countries'], function(Countries) {
      * shortcode, ID, and signature color.
      */
     parseURI: function(uri) {
-      var parseResult = { uri: uri };
+      var parseResult = { uri: uri },
+
+          uriToId = function(uri, pattern) {
+            var tokenPos = pattern.indexOf('\{\{id\}\}'),
+                suffix;
+
+            if (tokenPos === -1) {
+              // Suffix
+              return uri.substring(pattern.length);
+            } else {
+              // Infix
+              suffix = pattern.substring(tokenPos + 6);
+              return uri.substring(tokenPos, pattern.indexOf(suffix, tokenPos));
+            }
+          };
 
       jQuery.each(KNOWN_GAZETTEERS, function(i, g) {
         var cont = true;
         jQuery.each(g.url_patterns, function(j, pattern) {
-          if (uri.indexOf(pattern) === 0) {
+          var tokenPos = pattern.indexOf(TOKEN),
+              patternSuffix = (tokenPos === -1) ? pattern : pattern.substring(0, tokenPos);
+
+          if (uri.indexOf(patternSuffix) === 0) {
             parseResult.shortcode = g.shortcode;
-            parseResult.id = uri.substring(pattern.length);
+            parseResult.id = uriToId(uri, pattern);
             parseResult.color = g.color;
 
             cont = false;
