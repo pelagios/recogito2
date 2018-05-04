bb544ad3f4127d19eec3fb47b750402c553b8d95
Rainer Simon <rainer.simon@ait.ac.at>
2018-05-04 11:32:23 +0200

    Minor edits

--- app/assets/javascripts/document/annotation/text/relations/tagEditor.js
+++ app/assets/javascripts/document/annotation/text/relations/tagEditor.js
@@ -1,11 +1,30 @@
-define([], function() {
+define(['common/hasEvents'], function(HasEvents) {
 
   var TagEditor = function(containerEl, position) {
-    var element = jQuery(
-      '<div class="connection-editor-popup">Add tag...</div>').appendTo(containerEl);
+    var that = this,
 
-    element.css({ top: position[1] - 15, left: position[0] });
+        element = jQuery(
+          '<div class="connection-editor-popup">' +
+            '<input type="text" placeholder="Add tag..."></input>' +
+          '</div>').appendTo(containerEl),
+
+        inputEl = element.find('input'),
+
+        init = function() {
+          element.css({ top: position[1] - 15, left: position[0] });
+          setTimeout(function() { inputEl.focus(); }, 1);
+          inputEl.keyup(onKeyUp);
+        },
+
+        onKeyUp = function(e) {
+          if (e.keyCode == 13)
+            that.fireEvent('submit');
+        };
+
+    init();
+    HasEvents.apply(this);
   };
+  TagEditor.prototype = Object.create(HasEvents.prototype);
 
   return TagEditor;
 
