02b5768740dcff0fecd06bfb333137af392b156b
Rainer Simon <rainer.simon@ait.ac.at>
2018-05-23 09:04:38 +0200

    Refactoring

--- app/services/annotation/AnnotationService.scala
+++ app/services/annotation/AnnotationService.scala
@@ -6,6 +6,7 @@ import javax.inject.{Inject, Singleton}
 import org.joda.time.DateTime
 import play.api.Logger
 import scala.concurrent.{ExecutionContext, Future}
+import services.annotation.relation.RelationService
 import services.entity.builtin.{EntityService, IndexedEntity}
 import storage.es.ES
 
@@ -14,7 +15,7 @@ class AnnotationService @Inject() (
   entityService: EntityService,
   implicit val ctx: ExecutionContext,
   implicit val es: ES
-) extends HasAnnotationIndexing with AnnotationHistoryService {
+) extends HasAnnotationIndexing with AnnotationHistoryService with RelationService {
 
   /** Upserts an annotation.
     *  
@@ -140,47 +141,6 @@ class AnnotationService @Inject() (
       } start offset limit limit
     } map(_.to[(Annotation, Long)].toSeq)
     
-  
-  /** Fetch all annotations that have relations.
-    *  
-    * Works as a two-step process. First, we grab all source annotations using an 
-    * existsQuery. Then, we inspect the Ids of all destination annotations, and fetch 
-    * those that are missing. (I.e. those that are not source annotations themselves.)
-    */
-  def findWithRelationByDocId(id: String, offset: Int = 0, limit: Int = ES.MAX_SIZE): Future[Seq[Annotation]] = {
-    // Fetches source annotations from the index, i.e. those that have a 'relations' field
-    val fSourceAnnotations = es.client execute {
-      search(ES.RECOGITO / ES.ANNOTATION) query {
-        boolQuery
-          must (
-            termQuery("annotates.document_id" -> id),
-            existsQuery("relations.relates_to")
-          )
-      }
-    } map { _.to[(Annotation, Long)].toSeq.map(_._1) }
-
-    // Takes a list of source annotations, inspects destination Ids, and returns
-    // the Ids of annotations that are not yet included in the source annotations list
-    def determineMissingDestinations(sourceAnnotations: Seq[Annotation]) = {
-      val sourceIds = sourceAnnotations.map(_.annotationId)
-      val destinationIds = sourceAnnotations.flatMap(_.relations.map(_.relatesTo)).distinct
-      destinationIds diff sourceIds
-    }
-
-    // Batch-retrieves a list of annotations by their Ids
-    def findByIds(ids: Seq[UUID]) = es.client execute {
-      multiget (
-        ids.map { id => get(id.toString) from ES.RECOGITO / ES.ANNOTATION }
-      )
-    } map { _.to[(Annotation, Long)].toSeq.map(_._1) }
-    
-    for {
-      sourceAnnotations <- fSourceAnnotations
-      additionalDestinations <- findByIds(determineMissingDestinations(sourceAnnotations))
-    } yield (sourceAnnotations ++ additionalDestinations)
-  }
-    
-
   /** Deletes all annotations & version history on a given document **/
   def deleteByDocId(docId: String): Future[Boolean] = {
     val deleteAnnotations = findByDocId(docId).flatMap { annotationsAndVersions =>
