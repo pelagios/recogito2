21854b6584fa0c55b6528fc0fc757f6bb39a492a
Rainer Simon <rainer.simon@ait.ac.at>
2018-05-25 14:01:20 +0200

    I18N templates for annotation editor

---
+++ app/assets/javascripts/common/i18n/document/annotation/nls/bn/editor.js
@@ -0,0 +1,16 @@
+define({
+  'label_place': 'Place',
+  'label_person': 'Person',
+  'label_event': 'Event',
+
+  'placeholder_add_comment': 'Add a comment...',
+  'placeholder_add_tag': 'Add tag...',
+  'placeholder_transcribe': 'Transcribe...',
+
+  'dropdown_edit': 'Edit',
+  'dropdown_delete': 'Delete',
+
+  'btn_cancel': 'Cancel',
+  'btn_ok_and_next': 'OK &amp; Next',
+  'btn_ok': 'OK'
+});
