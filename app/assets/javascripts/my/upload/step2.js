f293a3d929c624d315dd8ee195692109faa8fa30
Rainer Simon <rainer.simon@ait.ac.at>
2018-05-29 11:16:23 +0200

    Enabled NER as selectable option for TEI upload

--- app/assets/javascripts/my/upload/step2.js
+++ app/assets/javascripts/my/upload/step2.js
@@ -49,8 +49,7 @@ require(['common/config'], function(Config) {
           var uploadsInList = jQuery('.dz-preview'),
               textUploads = jQuery.grep(uploadsInList, function(el) {
                 var contentType = jQuery(el).data('type');
-                // Note: we don't currently support NER for TEI text
-                return contentType && contentType.indexOf('TEXT_PLAIN') === 0;
+                return contentType && contentType.indexOf('TEXT_') === 0;
               });
 
           return textUploads.length > 0;
