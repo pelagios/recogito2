5a9b11047cc6a8c992f48bb9b5e1003be02557f2
Rainer Simon <rainer.simon@ait.ac.at>
2018-05-30 15:06:53 +0200

    Fixed privacy policy link

--- app/assets/javascripts/landing/index.js
+++ app/assets/javascripts/landing/index.js
@@ -48,8 +48,8 @@ require([], function() {
             },
             position: 'bottom-left',
             content: {
-              message: 'Recogito uses a cookie to remember your login state. The cookie is cleared from your browser when you log out.',
-              href: '/help/about#cookies'
+              message: 'Recogito uses a cookie to keep you logged in. The cookie is cleared from your browser when you log out.',
+              href: '/help/privacy'
             }
           });
         };
