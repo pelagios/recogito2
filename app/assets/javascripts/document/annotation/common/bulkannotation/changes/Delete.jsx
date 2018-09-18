85814ea6ab09141d3fc3ec158fd90435fdafb84c
Rainer Simon <rainer.simon@ait.ac.at>
2018-09-18 16:34:09 +0200

    Added 'Delete' indicator to bulk annotation dialog

---
+++ app/assets/javascripts/document/annotation/common/bulkannotation/changes/Delete.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ChangesOverview extends Component {
+
+  render() {
+    return(
+      <div className="delete">Delete</div>
+    )
+  }
+
+}
