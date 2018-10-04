a2f4930e83c6590c94798bc53eb9e9190f62ab55
Rainer Simon <rainer.simon@ait.ac.at>
2018-10-04 11:30:03 +0200

    Personal workspace API design

--- app/controllers/my/ng/WorkspaceAPIController.scala
+++ app/controllers/my/ng/WorkspaceAPIController.scala
@@ -3,11 +3,13 @@ package controllers.my.ng
 import com.mohiva.play.silhouette.api.Silhouette
 import controllers.{BaseController, HasPrettyPrintJSON, Security}
 import javax.inject.{Inject, Singleton}
+import org.joda.time.DateTime
 import play.api.Configuration
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 import play.api.mvc.{Action, ControllerComponents}
 import scala.concurrent.ExecutionContext
+import services.HasDate
 import services.document.DocumentService
 import services.generated.tables.records.{DocumentRecord, DocumentFilepartRecord}
 import services.user.UserService
@@ -24,7 +26,8 @@ class WorkspaceAPIController @Inject() (
     val config: Configuration,
     implicit val ctx: ExecutionContext
   ) extends BaseController(components, config, users)
-      with HasPrettyPrintJSON {
+      with HasPrettyPrintJSON 
+      with HasDate {
   
   // DocumentRecord JSON serialization     
   import services.document.DocumentService.documentRecordWrites
@@ -36,27 +39,61 @@ class WorkspaceAPIController @Inject() (
     t._1,
     t._2.map(_.getContentType)
   ))
-  
-  def me = Action.async { implicit request =>
-    users.findByUsername("rainer").map { user =>
-      val usedMB = Math.round(100 * uploads.getUsedDiskspaceKB("rainer") / 1024).toDouble / 100
-      import services.user.User._
+
+  def account = silhouette.SecuredAction.async { implicit request =>
+    val username = request.identity.username
+
+    val fUser = users.findByUsername(username)
+    val fMyDocCount = documents.countByOwner(username)
+    val fSharedCount = documents.countBySharedWith(username)
+
+    val f = for {
+      user <- fUser
+      myDocCount <- fMyDocCount
+      sharedCount <- fSharedCount
+    } yield (user.get, myDocCount, sharedCount)
+
+    f.map { case (user, myDocCount, sharedCount) =>
+      val usedMb = Math.round(100 * uploads.getUsedDiskspaceKB(username) / 1024).toDouble / 100
+
       val json = 
-        Json.toJson(user.get.record).as[JsObject] ++ Json.obj("used_mb" -> usedMB)
-      jsonOk(json).withHeaders("Access-Control-Allow-Origin" -> "*")
+        Json.obj(
+          "username" -> user.username,
+          "real_name" -> user.realName,
+          "member_since" -> formatDate(new DateTime(user.memberSince.getTime)),
+          "documents" -> Json.obj(
+            "my_documents" -> myDocCount,
+            "shared_with_me" -> sharedCount
+          ),
+          "storage" -> Json.obj(
+            "quota_mb" -> user.quotaMb.toInt,
+            "used_mb" -> usedMb
+          )
+        )
+
+      jsonOk(json).withHeaders(
+        "Access-Control-Allow-Origin" -> "http://localhost:7171",
+        "Access-Control-Allow-Credentials" -> "true"
+      )
     }
   }
   
   // Quick hack for testing  + CORS
-  def myDocuments(offset: Int, size: Int) = Action.async { implicit request =>
-    documents.findByOwnerWithPartMetadata("rainer", offset, size).map { documents =>
-      jsonOk(Json.toJson(documents.toSeq)).withHeaders("Access-Control-Allow-Origin" -> "*")
+  def myDocuments(offset: Int, size: Int) = silhouette.SecuredAction.async { implicit request =>
+    documents.findByOwnerWithPartMetadata(request.identity.username, offset, size).map { documents =>
+      jsonOk(Json.toJson(documents.toSeq)).withHeaders(
+        "Access-Control-Allow-Origin" -> "http://localhost:7171",
+        "Access-Control-Allow-Credentials" -> "true"
+      )
     }
   }
   
   def sharedWithMe(offset: Int, size: Int) = Action.async { implicit request =>
     documents.findBySharedWith("username", offset, size).map { documents =>
-      jsonOk(Json.toJson(documents.items.map(_._1))).withHeaders("Access-Control-Allow-Origin" -> "*")
+      jsonOk(Json.toJson(documents.items.map(_._1))).withHeaders(
+        "Access-Control-Allow-Origin" -> "http://localhost:7171",
+        "Access-Control-Allow-Credentials" -> "true"
+      )
     }
   }
 
