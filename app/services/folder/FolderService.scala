9455d5fa2a86f32ab1b037581b602cb2ccf0a2a3
Rainer Simon <rainer.simon@ait.ac.at>
2018-11-14 11:35:04 +0100

    Some base CRUD stuff for folders

---
+++ app/services/folder/FolderService.scala
@@ -0,0 +1,58 @@
+package services.folder
+
+import javax.inject.{Inject, Singleton}
+import play.api.Logger
+import scala.collection.JavaConversions._
+import scala.concurrent.Future
+import services.{Page, BaseService}
+import services.generated.Tables.FOLDER
+import services.generated.tables.records.FolderRecord
+import storage.db.DB
+
+@Singleton
+class FolderService @Inject() (implicit val db: DB) extends BaseService {
+
+  def listFolders(owner: String, offset: Int, size: Int): Future[Page[FolderRecord]] = 
+    db.query { sql => 
+      val startTime = System.currentTimeMillis
+
+      val total = sql.selectCount().from(FOLDER).fetchOne(0, classOf[Int])
+
+      val items = 
+        sql.selectFrom(FOLDER)
+           .where(FOLDER.OWNER.equal(owner))
+           .orderBy(FOLDER.TITLE.asc)
+           .limit(size)
+           .offset(offset)
+           .fetch()
+           .into(classOf[FolderRecord])
+
+      Page(System.currentTimeMillis - startTime, total, offset, size, items)
+    }  
+
+  def createFolder(owner: String, title: String, parent: Option[Int]): Future[FolderRecord] = 
+    db.withTransaction { sql => 
+      val folder = new FolderRecord(null, owner, title, optInt(parent))
+      sql.insertInto(FOLDER).set(folder).execute()
+      folder
+    }
+
+  def deleteFolder(id: Int): Future[Boolean] = 
+    db.withTransaction { sql => 
+      sql.deleteFrom(FOLDER).where(FOLDER.ID.equal(id)).execute == 1
+    }
+
+  def deleteByOwner(owner: String): Future[Boolean] = 
+    db.withTransaction { sql => 
+      sql.deleteFrom(FOLDER).where(FOLDER.OWNER.equal(owner)).execute == 1
+    }
+
+  def updateFolderTitle(id: Int, title: String): Future[Boolean] = 
+    db.withTransaction { sql => 
+      sql.update(FOLDER)
+        .set(FOLDER.TITLE, title)
+        .where(FOLDER.ID.equal(id))
+        .execute() == 1
+    }
+
+}
