44ac94c0c68f3b6d1fc891c9811cb80c17c16596
Rainer Simon <rainer.simon@ait.ac.at>
2018-12-05 11:33:48 +0100

    API method to list available NER plugins

---
+++ app/controllers/api/PluginAPIController.scala
@@ -0,0 +1,46 @@
+package controllers.api
+
+import controllers.{BaseOptAuthController, HasPrettyPrintJSON}
+import javax.inject.{Inject, Singleton}
+import org.pelagios.recogito.sdk.ner.NERPlugin
+import play.api.Configuration
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import play.api.mvc.ControllerComponents
+import scala.collection.JavaConverters._
+import scala.concurrent.ExecutionContext
+import services.document.DocumentService
+import services.user.UserService
+import transform.ner.NERPluginManager
+
+@Singleton
+class PluginAPIController @Inject() (
+  val components: ControllerComponents,
+  val config: Configuration,
+  val documents: DocumentService,
+  val users: UserService,
+  implicit val ctx: ExecutionContext
+) extends BaseOptAuthController(components, config, documents, users) with HasPrettyPrintJSON {
+
+  implicit val pluginWrites: Writes[NERPlugin] = (
+    (JsPath \ "identifier").write[String] and
+    (JsPath \ "name").write[String] and
+    (JsPath \ "languages").write[Seq[String]] and
+    (JsPath \ "organization").write[String] and
+    (JsPath \ "description").write[String] and 
+    (JsPath \ "version").write[String]
+  )(p => (
+     p.getClass.getName,
+     p.getName,
+     p.getSupportedLanguages.asScala,
+     p.getOrganization,
+     p.getDescription,
+     p.getVersion
+  ))
+
+  def listNERPlugins = Action { implicit request =>
+    val plugins = NERPluginManager.plugins
+    jsonOk(Json.toJson(plugins))
+  }
+
+}
---
+++ plugins/org.pelagios/recogito.plugins.ner.mock/readme.md
@@ -0,0 +1,6 @@
+# Mock NER plugin
+
+A dummy plugin, just for the purpose of implementing/testing the 
+NER plugin selection UI.
+
+Run `sbt package` to compile the plugin.jar from source.
