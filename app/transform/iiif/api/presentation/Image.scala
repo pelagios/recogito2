a4f863ad8d88db9c244616f15e591117e9d0d9e6
Rainer Simon <rainer.simon@ait.ac.at>
2019-02-22 08:30:47 +0100

    Supporting BL IIIF presentation manifest flavour

--- app/transform/iiif/api/presentation/Image.scala
+++ app/transform/iiif/api/presentation/Image.scala
@@ -8,8 +8,10 @@ case class Image(width: Int, height: Int, service: String)
 object Image {
   
   implicit val imageReads: Reads[Image] = (
-    (JsPath \ "resource" \ "width").read[Int] and
-    (JsPath \ "resource" \ "height").read[Int] and
+    (JsPath \ "resource" \ "width").readNullable[Int] and
+    (JsPath \ "resource" \ "height").readNullable[Int] and
+    (JsPath \ "resource" \ "service" \ "width").readNullable[Int] and
+    (JsPath \ "resource" \ "service" \ "height").readNullable[Int] and
     (JsPath \ "resource" \ "service" \ "@id").read[String]
       .map { url =>
         // Some manifests omit the info.json suffix, but then 
@@ -18,6 +20,19 @@ object Image {
         else if (url.endsWith("/")) s"${url}info.json"
         else s"${url}/info.json"
       }
-  )(Image.apply _)
+  ).tupled.map(t => Image.apply(t._1, t._2, t._3, t._4, t._5))
+
+  def apply(
+    resourceWidth: Option[Int], 
+    resourceHeight: Option[Int],
+    serviceWidth: Option[Int],
+    serviceHeight: Option[Int],
+    service: String
+  ) = {
+    // Pick the first available, but fail hard if none at all
+    val width = Seq(resourceWidth, serviceWidth).flatten.head
+    val height = Seq(resourceHeight, serviceHeight).flatten.head
+    new Image(width, height, service)
+  }
   
 }
