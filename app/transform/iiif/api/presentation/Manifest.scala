d910d9863259916f157e7b9caa0da643cad04f95
Rainer Simon <rainer.simon@ait.ac.at>
2019-02-26 09:15:43 +0100

    IIIF manifest parser bugfix

--- app/transform/iiif/api/presentation/Manifest.scala
+++ app/transform/iiif/api/presentation/Manifest.scala
@@ -2,6 +2,7 @@ package transform.iiif.api.presentation
 
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
+import transform.iiif.api.PlainLiteral
 
 case class Manifest(
   label: Option[String],
@@ -10,7 +11,7 @@ case class Manifest(
 object Manifest {
   
   implicit val manifestReads: Reads[Manifest] = (
-    (JsPath \ "label").readNullable[String] and
+    (JsPath \ "label").readNullable[PlainLiteral].map(_.map(_.value)) and
     (JsPath \ "sequences").read[Seq[Sequence]]
   )(Manifest.apply _)
   
