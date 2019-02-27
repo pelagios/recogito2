60ceaebf5c47563f3923e1952b0c722fef09cb84
Rainer Simon <rainer.simon@ait.ac.at>
2019-02-27 15:19:46 +0100

    Cleanup

--- app/controllers/admin/AdminController.scala
+++ app/controllers/admin/AdminController.scala
@@ -31,12 +31,10 @@ class AdminController @Inject() (
 ) extends BaseAuthController(components, config, documents, users) with HasPrettyPrintJSON {
         
   def index = silhouette.SecuredAction(Security.WithRole(Admin)) { implicit request =>
-    // Ok(views.html.admin.index())
     Ok(views.html.admin.activity())
   }
   
   def getStats() = silhouette.SecuredAction(Security.WithRole(Admin)).async { implicit request =>
-    
     // DocumentRecord JSON serialization
     import DocumentService._
     
