12484fc16159f64a787d0d8460286fafeaf088af
Rainer Simon <rainer.simon@ait.ac.at>
2019-05-23 10:36:12 +0200

    Bugfix

--- app/controllers/my/directory/create/CreateController.scala
+++ app/controllers/my/directory/create/CreateController.scala
@@ -199,7 +199,7 @@ class CreateController @Inject() (
     documents.getExtendedMeta(id: String, Some(request.identity.username)).flatMap { _ match { 
       case Some((doc, accesslevel)) =>
         val newOwner = 
-          if (doc.owner == request.identity.username)
+          if (doc.owner.getUsername == request.identity.username)
             None // current user is document owner - just a duplicate, no fork
           else 
             Some(request.identity) // Fork to current user workspace
