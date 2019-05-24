345ba72adcd638469674751b1296e1865f42bbb3
Rainer Simon <rainer.simon@ait.ac.at>
2019-05-24 09:42:34 +0200

    More work on advanced search query builder

--- app/services/document/search/SearchOps.scala
+++ app/services/document/search/SearchOps.scala
@@ -9,10 +9,66 @@ import services.generated.Tables.{SHARING_POLICY, DOCUMENT}
 
 trait SearchOps { self: DocumentService => 
 
-  def search(loggedInAs: Option[String], args: SearchArgs) = db.query { sql =>
+  private def render(q: String, vars: String*) = {
+    val withBrackets = condition(q, vars:_*).toString
+    // Bit of a hack... JOOQ wraps conditions in brackets and we need to get rid of them
+    withBrackets.substring(1, withBrackets.size - 1)
+  }
+
+  /** Appends the query phrase, if any */
+  private def appendQueryPhrase(args: SearchArgs): String =>  String = query =>
+    args.query match {
+      case Some(phrase) => render(
+        s"""
+         $query 
+           AND lower(document.title) LIKE ?
+         """,
+        s"%${phrase.toLowerCase}%")
+
+      case None => query
+    }
+
+  /** Appends the scope filter part **/
+  private def appendScope(loggedInAs: Option[String], args: SearchArgs): String => String = query =>
+    (loggedInAs, args.searchIn) match {
+      case (None, _) => // No matter what 'searchIn' says, anonymous visitors only get public docs
+        s"""
+         $query
+         WHERE document.public_visibility = 'PUBLIC'
+         """
+
+      case (Some(username), Scope.ALL) => render(
+        s"""
+         $query
+           LEFT OUTER JOIN sharing_policy
+             ON sharing_policy.document_id = document.id
+         WHERE (
+           document.owner = ?
+           OR document.public_visibility = 'PUBLIC'
+           OR sharing_policy.shared_with = ?
+         )
+         """, username, username)
+
+      case (Some(username), Scope.MY) => render(
+        s"""
+         $query
+         WHERE document.owner = ?
+         """, username)
+
+      case (Some(username), Scope.SHARED) => render(
+        s"""
+         $query
+           JOIN sharing_policy
+             ON sharing_policy.document_id = document.id
+         WHERE sharing_policy.shared_with = ?
+         """, username)
+    }
+
+  def search(loggedInAs: Option[String], args: SearchArgs) = db.query { sql => 
     val startTime = System.currentTimeMillis
 
-    val baseSelect =
+    // The base component for the DB query
+    val base =
       """
       SELECT
         document.*,
@@ -29,99 +85,13 @@ trait SearchOps { self: DocumentService =>
         ) AS parts ON parts.document_id = document.id
       """
 
-    val baseQuery = (loggedInAs, args.searchIn) match {
-      case (None, _) => // No matter what 'searchIn' says, anonymous visitors only get public docs
-        s"""
-         ${baseSelect}
-         WHERE document.public_visibility = 'PUBLIC' ;
-         """
-
-      case (Some(username), Scope.ALL) => 
-        s"""
-         $baseSelect
-           LEFT OUTER JOIN sharing_policy
-             ON sharing_policy.document_id = document.id
-         WHERE document.owner = '$username'
-           OR document.public_visibility = 'PUBLIC'
-           OR sharing_policy.shared_with = '$username' ;
-         """
-
-      case (Some(username), Scope.MY) =>
-        s"""
-         $baseSelect
-         WHERE document.owner = '$username' ;
-         """
-
-      case (Some(username), Scope.SHARED) =>
-        s"""
-         ${baseSelect}
-           JOIN sharing_policy
-             ON sharing_policy.document_id = document.id
-         WHERE sharing_policy.shared_with = '$username' ;
-         """
-    }
+    val buildQuery = 
+      appendScope(loggedInAs, args) andThen 
+      appendQueryPhrase(args)
 
     // TODO just a hack for now
-    val documents = sql.resultQuery(baseQuery).fetchArray.map(MyDocument.build)
+    val documents = sql.resultQuery(buildQuery(base)).fetchArray.map(MyDocument.build)
     Page(System.currentTimeMillis - startTime, documents.size, 0, documents.size, documents)
   }
-
-  /*
-  def oldSearch(loggedInAs: Option[String], args: SearchArgs) = db.query { sql => 
-    val startTime = System.currentTimeMillis
-
-    val q = loggedInAs match {
-
-      case Some(username) => 
-        // TODO include shared documents
-        val q = 
-          s"""
-            SELECT 
-              document.*,
-              file_count,
-              content_types
-            FROM document
-              LEFT JOIN (
-                SELECT
-                  count(*) AS file_count,
-                  array_agg(DISTINCT content_type) AS content_types,
-                  document_id
-                FROM document_filepart
-                GROUP BY document_id
-              ) AS parts ON parts.document_id = document.id
-            WHERE document.public_visibility = 'PUBLIC'
-              AND lower(document.title) LIKE ?;
-            """
-
-        sql.resultQuery(q, s"%${args.query.get.toLowerCase}%")
-
-      case None => 
-        val q = 
-          s"""
-            SELECT 
-              document.*,
-              file_count,
-              content_types
-            FROM document
-              LEFT JOIN (
-                SELECT
-                  count(*) AS file_count,
-                  array_agg(DISTINCT content_type) AS content_types,
-                  document_id
-                FROM document_filepart
-                GROUP BY document_id
-              ) AS parts ON parts.document_id = document.id
-            WHERE document.public_visibility = 'PUBLIC'
-              AND lower(document.title) LIKE ?;
-            """
-
-        sql.resultQuery(q, s"%${args.query.get.toLowerCase}%")
-
-    }
-
-    val documents = q.fetchArray.map(MyDocument.build)
-    Page(System.currentTimeMillis - startTime, documents.size, 0, documents.size, documents)
-  }
-  */
 
 }
