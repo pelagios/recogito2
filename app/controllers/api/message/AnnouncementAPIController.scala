0677c0978243690908898d7129c1ff834433628a
Rainer Simon <rainer.simon@ait.ac.at>
2019-08-05 11:26:34 +0200

    Personal message prototype

--- app/controllers/api/message/AnnouncementAPIController.scala
+++ app/controllers/api/message/AnnouncementAPIController.scala
@@ -7,11 +7,12 @@ import javax.inject.{Inject, Singleton}
 import play.api.Configuration
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
+import play.api.libs.mailer._
 import play.api.mvc.ControllerComponents
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import services.announcement.AnnouncementService
 import services.document.DocumentService
-import services.user.UserService
+import services.user.{User, UserService}
 
 @Singleton
 class AnnouncementAPIController @Inject() (
@@ -21,6 +22,7 @@ class AnnouncementAPIController @Inject() (
   val documents: DocumentService,
   val silhouette: Silhouette[Security.Env],
   val users: UserService,
+  val mailerClient: MailerClient,
   implicit val ctx: ExecutionContext
 ) extends BaseAuthController(components, config, documents, users) with HasPrettyPrintJSON {
 
@@ -39,6 +41,50 @@ class AnnouncementAPIController @Inject() (
   def confirm(id: UUID) = silhouette.SecuredAction.async { implicit request =>
     announcements.confirm(id, request.identity.username, "OK").map { success => 
       if (success) Ok else BadRequest
+    }
+  }
+
+  def sendMessage() = silhouette.SecuredAction.async { implicit request => 
+
+    // Does the actual sending
+    def send(sender: String, recipient: User, message: String) = {
+      // TODO this now hard-wires "noreply@pelagios.org" as reply address
+      // TODO see if we can take this directly from the config file instead
+      val email = Email(
+        s"[Recogito] Message from user $sender",
+        "Recogito Team <noreply@pelagios.org>",
+        Seq(users.decryptEmail(recipient.email)),
+        Some(message) // TODO wrap in styled "envelope"
+      )
+
+      play.api.Logger.info(s"Message from $sender to ${recipient.username}: $message")      
+      mailerClient.send(email)
+    }
+
+    // Request validation and error handling
+    request.body.asJson match {
+      case Some(json) =>
+        val maybeTo: Option[String] = (json \ "to").asOpt[String]
+        val maybeMessage: Option[String] = (json \ "message").asOpt[String]
+
+        (maybeTo, maybeMessage) match {
+          case (Some(toUser), Some(message)) => 
+            users.findByUsername(toUser).map { _ match {
+              case Some(user) => 
+                // TODO check if recipient != sender (but leave open for testing for now)
+                send(request.identity.username, user, message)
+                Ok
+
+              case None => 
+                NotFound // Unknown recipient name
+            }}
+
+          case _ =>
+            Future.successful(BadRequest) // Lacks recipient name, message, or both
+        }
+
+      case None => 
+        Future.successful(BadRequest) // No JSON payload
     }
   }
 
