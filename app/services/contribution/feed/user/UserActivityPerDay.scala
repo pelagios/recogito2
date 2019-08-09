976a24c31710b49e911b686ef2465157b1a6e7bc
Rainer Simon <rainer.simon@ait.ac.at>
2019-08-09 08:36:24 +0200

    Activity feed serialization fix

--- app/services/contribution/feed/user/UserActivityPerDay.scala
+++ app/services/contribution/feed/user/UserActivityPerDay.scala
@@ -4,13 +4,16 @@ import org.joda.time.DateTime
 import play.api.mvc.{AnyContent, Request}
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
-import services.{HasDate, RuntimeAccessLevel}
+import services.{HasDate, HasNullableSeq, RuntimeAccessLevel}
 import services.generated.tables.records.DocumentRecord
 
 case class UserActivityPerDay(timestamp: DateTime, count: Long, users: Seq[ActivityPerUser]) {
 
   def filter(docsAndPermissions: Seq[(DocumentRecord, RuntimeAccessLevel)]) = {
-    val filteredUsers = users.map(_.filter(docsAndPermissions))
+    val filteredUsers = users
+      .map(_.filter(docsAndPermissions)) // May now contain users with 0 visible edits - remove
+      .filter(_.count > 0)
+
     val filteredCount = filteredUsers.map(_.count).sum
     UserActivityPerDay(timestamp, filteredCount, filteredUsers)
   }
@@ -20,12 +23,13 @@ case class UserActivityPerDay(timestamp: DateTime, count: Long, users: Seq[Activ
     
 }
 
-object UserActivityPerDay extends HasDate {
+object UserActivityPerDay extends HasDate with HasNullableSeq {
 
   implicit val userActivityPerDayWrites: Writes[UserActivityPerDay] = (
     (JsPath \ "timestamp").write[DateTime] and
     (JsPath \ "contributions").write[Long] and
-    (JsPath \ "users").write[Seq[ActivityPerUser]]
+    (JsPath \ "users").writeNullable[Seq[ActivityPerUser]]
+      .contramap(toOptSeq[ActivityPerUser])
   )(unlift(UserActivityPerDay.unapply))
 
 }
