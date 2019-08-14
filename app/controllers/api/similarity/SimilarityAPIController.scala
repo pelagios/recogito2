9f7f4b7b13bd8ef7ed03e090437278a3c80c1251
Rainer Simon <rainer.simon@ait.ac.at>
2019-08-14 12:19:49 +0200

    Some proof-of-concept-ing

--- app/controllers/api/similarity/SimilarityAPIController.scala
+++ app/controllers/api/similarity/SimilarityAPIController.scala
@@ -6,13 +6,13 @@ import javax.inject.{Inject, Singleton}
 import play.api.Configuration
 import play.api.libs.json.Json
 import play.api.mvc.ControllerComponents 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import services.document.DocumentService
 import services.user.UserService
 import services.similarity.SimilarityService
 
 @Singleton
-class ContributionAPIController @Inject() (
+class SimilarityAPIController @Inject() (
   val components: ControllerComponents,
   val config: Configuration,
   val documents: DocumentService,
@@ -24,7 +24,16 @@ class ContributionAPIController @Inject() (
 
   def getSimilar(docId: String) = silhouette.UserAwareAction.async { implicit request => 
     documentResponse(docId, request.identity, { case (doc, accesslevel) => 
-      ???
+      if (accesslevel.canReadData) {
+        similarities.findSimilar(docId, request.identity.map(_.username)).map { response => 
+          response.foreach { tuple => 
+            play.api.Logger.info(tuple.toString)
+          }
+          Ok
+        }
+      } else {
+        Future.successful(Forbidden)
+      }
     })
   }
   
--- app/services/similarity/SimilarityService.scala
+++ app/services/similarity/SimilarityService.scala
@@ -2,15 +2,18 @@ package services.similarity
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext
+import services.document.DocumentService
 import services.generated.Tables.SIMILARITY
 import services.generated.tables.records.SimilarityRecord
 import storage.db.DB
 
-@Singleton
-class SimilarityService @Inject() (val db: DB, implicit val ctx: ExecutionContext) {
+/** A helper to work with SimilarityRecords more easily **/
+case class Similarity(docId: String, jaroWinkler: Double, jaccard: Double)
 
-  /** Query the similarity metrics table for the top N most similar documents **/
-  def findSimilar(docId: String, n: Int = 10) = db.query { sql => 
+@Singleton
+class SimilarityService @Inject() (val db: DB, val documents: DocumentService, implicit val ctx: ExecutionContext) {
+
+  private def fetchSimilarityMetrics(docId: String, n: Int = 10) = db.query { sql =>
     val query = 
       s"""
        SELECT
@@ -22,8 +25,39 @@ class SimilarityService @Inject() (val db: DB, implicit val ctx: ExecutionContex
        LIMIT ${n}
        """
 
-    sql.resultQuery(query, docId, docId).fetchArray.map { record => 
-      record.into(classOf[SimilarityRecord])
+    sql.resultQuery(query, docId, docId).fetchArray.map { row => 
+      val r = row.into(classOf[SimilarityRecord])
+      
+      val docA = r.getDocIdA
+      val docB = r.getDocIdB
+
+      // TODO remove the need for Option by adding DEFAULT = 0 to schema
+      val jw = Option(r.getTitleJaroWinkler).map(_.toDouble).getOrElse(0.0)
+      val jc = Option(r.getEntityJaccard).map(_.toDouble).getOrElse(0.0)
+
+      if (docA == docId)
+        Similarity(docB, jw, jc)
+      else 
+        Similarity(docA, jw, jc)
+    }
+  }
+
+  /** Query the similarity metrics table for the top N most similar documents 
+    *
+    * Note that this could be rolled into one query (similarity, document metadata 
+    * and access permissions). But things get really unreadable. So we split it into 
+    * two queries (and re-use the document service for the second part.)
+    */
+  def findSimilar(docId: String, loggedInAs: Option[String], n: Int = 10) = {
+    val f = for {
+      similarities <- fetchSimilarityMetrics(docId, n)
+      docsAndAccesslevels <- documents.getDocumentRecordsByIdWithAccessLevel(similarities.map(_.docId), loggedInAs)
+    } yield (similarities, docsAndAccesslevels)
+
+    f.map { case (similarities, docsAndAccessLevels) => 
+      docsAndAccessLevels.map { case (doc, accesslevel) => 
+        (doc, accesslevel, similarities.find(_.docId == doc.getId).get)
+      } filter { _._2.canReadData } // Only expose docs with at least READ_DATA permission
     }
   }
 
