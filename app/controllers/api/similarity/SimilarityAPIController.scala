1798d4c7d6e7f1326c3054280d4c6f09b5400b74
Rainer Simon <rainer.simon@ait.ac.at>
2019-08-14 13:09:41 +0200

    Basic JSON similarity API

--- app/controllers/api/similarity/SimilarityAPIController.scala
+++ app/controllers/api/similarity/SimilarityAPIController.scala
@@ -4,12 +4,15 @@ import com.mohiva.play.silhouette.api.Silhouette
 import controllers.{BaseOptAuthController, Security, HasPrettyPrintJSON}
 import javax.inject.{Inject, Singleton}
 import play.api.Configuration
-import play.api.libs.json.Json
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 import play.api.mvc.ControllerComponents 
 import scala.concurrent.{ExecutionContext, Future}
-import services.document.DocumentService
+import services.RuntimeAccessLevel
+import services.document.{DocumentService, ExtendedDocumentMetadata}
+import services.generated.tables.records.DocumentRecord
 import services.user.UserService
-import services.similarity.SimilarityService
+import services.similarity.{Similarity, SimilarityService}
 
 @Singleton
 class SimilarityAPIController @Inject() (
@@ -22,14 +25,41 @@ class SimilarityAPIController @Inject() (
   implicit val ctx: ExecutionContext
 ) extends BaseOptAuthController(components, config, documents, users) with HasPrettyPrintJSON {
 
+  implicit val similarDocumentWrites: Writes[(DocumentRecord, RuntimeAccessLevel, Similarity)] = (
+    (JsPath \ "document_id").write[String] and
+    (JsPath \ "author").writeNullable[String] and
+    (JsPath \ "title").write[String] and 
+    (JsPath \ "owner").write[String] and
+    (JsPath \ "similarity").write[JsValue]
+  )(t => (
+      t._1.getId, 
+      Option(t._1.getAuthor),
+      t._1.getTitle,
+      t._1.getOwner,
+      Json.obj(
+        "title" -> t._3.jaroWinkler, 
+        "entities" -> t._3.jaccard)
+  ))
+
+  implicit val similarityAPIResponseWrites: Writes[(ExtendedDocumentMetadata, Seq[(DocumentRecord, RuntimeAccessLevel, Similarity)])] = (
+    (JsPath \ "document_id").write[String] and
+    (JsPath \ "author").writeNullable[String] and
+    (JsPath \ "title").write[String] and 
+    (JsPath \ "owner").write[String] and 
+    (JsPath \ "similar").write[Seq[(DocumentRecord, RuntimeAccessLevel, Similarity)]]
+  )(t => (
+      t._1.id,
+      t._1.author,
+      t._1.title,
+      t._1.ownerName,
+      t._2
+  ))
+
   def getSimilar(docId: String) = silhouette.UserAwareAction.async { implicit request => 
     documentResponse(docId, request.identity, { case (doc, accesslevel) => 
       if (accesslevel.canReadData) {
         similarities.findSimilar(docId, request.identity.map(_.username)).map { response => 
-          response.foreach { tuple => 
-            play.api.Logger.info(tuple.toString)
-          }
-          Ok
+          jsonOk(Json.toJson((doc, response)))
         }
       } else {
         Future.successful(Forbidden)
