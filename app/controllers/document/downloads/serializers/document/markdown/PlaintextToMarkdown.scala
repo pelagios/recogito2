599326936f002ea4325a281f5a6b06e2b5d363da
Rainer Simon <rainer.simon@ait.ac.at>
2019-11-27 16:14:24 +0100

    MD export work in progress

--- app/controllers/document/downloads/serializers/document/markdown/PlaintextToMarkdown.scala
+++ app/controllers/document/downloads/serializers/document/markdown/PlaintextToMarkdown.scala
@@ -2,14 +2,25 @@ package controllers.document.downloads.serializers.document.markdown
 
 import controllers.document.downloads.serializers.BaseSerializer
 import scala.concurrent.{ExecutionContext, Future}
-import services.annotation.AnnotationService
+import services.annotation.{Annotation, AnnotationService}
 import services.document.{DocumentService, ExtendedDocumentMetadata}
 import services.entity.builtin.EntityService
+import services.generated.tables.records.DocumentFilepartRecord
 import storage.uploads.Uploads
 
 trait PlaintextToMarkdown extends BaseSerializer {
 
-  private def textToMarkdown() = ???
+  private def textToMarkdown(
+    text: String, 
+    annotations: Seq[Annotation]
+  ) = {
+
+    // TODO 'interleave' - place a footnote marker at the end of each annotation
+    text
+
+    // TODO append annotation content as footnote
+
+  }
 
   def plaintextToMarkdown(
     doc: ExtendedDocumentMetadata
@@ -20,20 +31,34 @@ trait PlaintextToMarkdown extends BaseSerializer {
       ctx: ExecutionContext
   ) = {
     // Fetch plaintexts
-    val fTexts = Future.sequence {
+    val fTexts: Future[Seq[(String, DocumentFilepartRecord)]] = Future.sequence {
       doc.fileparts.map { part =>
         uploads.readTextfile(doc.owner.getUsername, doc.id, part.getFile).map(_.map((_, part)))
       }
     } map { _.flatten }
 
-    val header = """
-    |--------------|-----------|
-    |              |           |
-    |--------------|-----------|
-    """
+    val fAnnotations = annotationService.findByDocId(doc.id)
 
-    fTexts.map { _.foldLeft(header) { case (md, (plaintext, part)) => 
-      md + plaintext
+    val header =
+      """|--------------|-----------|
+        >|              |           |
+        >|--------------|-----------|
+        >
+        >""".stripMargin('>')
+
+    val f = for {
+      texts <- fTexts
+      annotations <- fAnnotations
+    } yield (texts, annotations.map(_._1))
+
+    f.map { case(texts, annotations) => {
+      texts.foldLeft(header) { case (md, (plaintext, part)) => 
+        val annotationsOnPart = sortByCharOffset(
+          annotations.filter(_.annotates.filepartId == part.getId)
+        )
+
+        md + textToMarkdown(plaintext, annotationsOnPart)
+      }
     }}
   }
 
