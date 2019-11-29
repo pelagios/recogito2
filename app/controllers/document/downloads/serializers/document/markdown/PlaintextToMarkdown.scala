426adf019480402f668fc3b441021107edba5eed
Rainer Simon <rainer.simon@ait.ac.at>
2019-11-29 09:50:00 +0100

    Bugfixes

--- app/controllers/document/downloads/serializers/document/markdown/PlaintextToMarkdown.scala
+++ app/controllers/document/downloads/serializers/document/markdown/PlaintextToMarkdown.scala
@@ -10,29 +10,54 @@ import storage.uploads.Uploads
 
 trait PlaintextToMarkdown extends BaseSerializer {
 
+  private def getStartOffset(a: Annotation) = a.anchor.substring(12).toInt
+
   private def getEndOffset(a: Annotation) = a.anchor.substring(12).toInt  + getFirstQuote(a).map(_.size).getOrElse(0)
 
   private def hasComments(a: Annotation) = getCommentBodies(a).length > 0
+
+  private def removeOverlappingAnnotations(annotations: Seq[Annotation]) = 
+    sort(annotations).foldLeft(Seq.empty[Annotation]) { case (result, next) =>
+      result.lastOption match {
+        case Some(previous) =>
+          // Check if next overlaps previous
+          val previousEnd = getEndOffset(previous)          
+          val nextStart = getStartOffset(next)
+          
+          if (nextStart <= previousEnd)
+            result :+ next
+          else
+            result
+          
+        case None =>
+          // First in line
+          Seq(next)
+      }
+    }
 
   private def textToMarkdown(
     text: String, 
     annotations: Seq[Annotation]
   ) = {
-    if (annotations.length > 0) {
-      val mdBody = annotations.foldLeft(("", 0)) { case((md, cursor), a) =>
+    val nonOverlapping = removeOverlappingAnnotations(annotations)
+
+    if (nonOverlapping.length > 0) {
+      val mdBody = nonOverlapping.foldLeft(("", 0)) { case((md, cursor), a) =>
         val textBefore = text.substring(cursor, getEndOffset(a))
         if (hasComments(a))
-          (s"$textBefore[^${a.annotationId}]", cursor + textBefore.length)
+          (s"$md$textBefore[^${a.annotationId}]", cursor + textBefore.length)
         else
           (s"$md$textBefore", cursor + textBefore.length)
-      }._1
+      }
 
-      val mdFootnotes = annotations.filter(hasComments).map { a => 
+      val mdSuffix = text.substring(mdBody._2)
+
+      val mdFootnotes = nonOverlapping.filter(hasComments).map { a => 
         val comments = getCommentBodies(a).flatMap(_.value).mkString("\n\n")
         s"[^${a.annotationId}]: ${comments}"
       }
 
-      s"${mdBody}\n\n${mdFootnotes.mkString("\n\n")}\n\n"
+      s"${mdBody._1}${mdSuffix}\n\n${mdFootnotes.mkString("\n\n")}\n\n"
     } else {
       text
     }
