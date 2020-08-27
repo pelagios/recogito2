43be0d804dd814ae7a75a556bf8abb654599fa62
Rainer Simon <rainer.simon@ait.ac.at>
2020-08-27 08:50:33 +0200

    Added label property to WebAnno JSON-LD export

--- app/controllers/document/downloads/serializers/annotations/webannotation/WebAnnotationTarget.scala
+++ app/controllers/document/downloads/serializers/annotations/webannotation/WebAnnotationTarget.scala
@@ -7,7 +7,7 @@ import services.ContentType
 import services.annotation.Annotation
 import services.generated.tables.records.DocumentFilepartRecord
 
-case class WebAnnotationTarget(source: String, hasType: String, selectors: Seq[WebAnnotationSelector])
+case class WebAnnotationTarget(source: String, hasType: String, label: String, selectors: Seq[WebAnnotationSelector])
 
 object WebAnnotationTarget {
 
@@ -31,23 +31,23 @@ object WebAnnotationTarget {
     annotation.annotates.contentType match {
       // Plaintest gets a Text target with two alternative selectors: TextQuote + TextPosition
       case TEXT_PLAIN =>
-        WebAnnotationTarget(source, "Text", Seq(
+        WebAnnotationTarget(source, "Text", filepart.getTitle, Seq(
           TextPositionSelector.fromAnnotation(annotation),
           TextQuoteSelector.fromAnnotation(annotation)))
 
       // TODO TEI gets a Text target with two alternative selectors: TextQuote + XPath Range
       case TEXT_TEIXML =>
-        WebAnnotationTarget(source, "Text", Seq(
+        WebAnnotationTarget(source, "Text", filepart.getTitle, Seq(
           TextQuoteSelector.fromAnnotation(annotation),
           XPathRangeSelector.fromAnnotation(annotation)))
 
       // Images get an Image target with a fragment selector
       case IMAGE_UPLOAD | IMAGE_IIIF =>
-        WebAnnotationTarget(source, "Image", Seq(ImageFragmentSelector.fromAnnotation(annotation)))
+        WebAnnotationTarget(source, "Image", filepart.getTitle, Seq(ImageFragmentSelector.fromAnnotation(annotation)))
 
       // CSV gets a Dataset target with a Data Position selector
       case DATA_CSV  =>
-        WebAnnotationTarget(source, "Dataset", Seq(TableFragmentSelector.fromAnnotation(annotation)))
+        WebAnnotationTarget(source, "Dataset", filepart.getTitle, Seq(TableFragmentSelector.fromAnnotation(annotation)))
 
       case c =>
         // Should never happen
@@ -59,7 +59,8 @@ object WebAnnotationTarget {
   implicit val webAnnotationTargetWrites: Writes[WebAnnotationTarget] = (
     (JsPath \ "source").write[String] and
     (JsPath \ "type").write[String] and
+    (JsPath \ "label").write[String] and
     (JsPath \ "selector").write[Seq[WebAnnotationSelector]]
-  )(t => (t.source, t.hasType, t.selectors))
+  )(t => (t.source, t.hasType, t.label, t.selectors))
 
 }
