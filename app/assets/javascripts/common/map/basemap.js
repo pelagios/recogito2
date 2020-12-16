8f5f9b402fb9400910a38100338979becb103cf0
Rainer Simon <aboutgeo@gmail.com>
2020-12-16 13:16:19 +0100

    Temporary removal of AWMC baselayer while it's offline

--- app/assets/javascripts/common/map/basemap.js
+++ app/assets/javascripts/common/map/basemap.js
@@ -8,31 +8,33 @@ define(['common/map/layerswitcher'], function(LayerSwitcher) {
 
     var Layers =  {
 
-          DARE   : L.tileLayer('http:///dh.gu.se/tiles/imperium/{z}/{x}/{y}.png', {
-                     attribution: 'Tiles: <a href="http://imperium.ahlfeldt.se/">DARE 2014</a>',
-                     minZoom:3,
-                     maxZoom:11
-                   }),
-
+          /*
           AWMC   : L.tileLayer('http://a.tiles.mapbox.com/v3/isawnyu.map-knmctlkh/{z}/{x}/{y}.png', {
                      attribution: 'Tiles &copy; <a href="http://mapbox.com/" target="_blank">MapBox</a> | ' +
                        'Data &copy; <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, CC-BY-SA | '+
                        'Tiles and Data &copy; 2013 <a href="http://www.awmc.unc.edu" target="_blank">AWMC</a> ' +
                        '<a href="http://creativecommons.org/licenses/by-nc/3.0/deed.en_US" target="_blank">CC-BY-NC 3.0</a>'
                    }),
+          */
 
           OSM    : L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                      attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
                    }),
 
+          DARE   : L.tileLayer('http:///dh.gu.se/tiles/imperium/{z}/{x}/{y}.png', {
+            attribution: 'Tiles: <a href="http://imperium.ahlfeldt.se/">DARE 2014</a>',
+            minZoom:3,
+            maxZoom:11
+          }),
+
           AERIAL : L.tileLayer('http://api.tiles.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicGVsYWdpb3MiLCJhIjoiMWRlODMzM2NkZWU3YzkxOGJkMDFiMmFiYjk3NWZkMmUifQ.cyqpSZvhsvBGEBwRfniVrg', {
                      attribution: '<a href="https://www.mapbox.com/about/maps/">&copy; Mapbox</a> <a href="http://www.openstreetmap.org/about/">&copy; OpenStreetMap</a>',
                      maxZoom:22
                    })
-
         },
 
-        currentBaseLayer = Layers.AWMC,
+        // currentBaseLayer = Layers.AWMC,
+        currentBaseLayer = Layers.OSM,
 
         map = L.map(element[0], {
           center: DEFAULT_CENTER,
