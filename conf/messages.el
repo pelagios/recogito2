b28427322d4ac6f6441ba7cf1e622315fcbf2d2c
kiolalis <kiolalis@gmail.com>
2022-03-23 22:37:03 +0200

    Update messages.el

--- conf/messages.el
+++ conf/messages.el
@@ -44,27 +44,34 @@ landing.index.registration_form.placeholder.register=Εγγραφείτε στο
 landing.index.registration_form.disclaimer.text=Συμφωνήστε με τους <a href="{0}">Όρους και Προϋποθέσεις Χρήσης</a> \
   και την αποθήκευση της διεύθυνσης ηλεκτρονικού ταχυδρομείου σας.
 
+# News
+landing.index.news.title=Τίτλος
+landing.index.news.subtitle=Υπότιτλος
+landing.index.news.text=Είμαστε ενθουσιασμένοι που ανακοινώνουμε ότι το Recogito έλαβε τον τίτλο του <strong>Καλύτερο Ψηφιακού \
+  Εργαλείου Ανθρωπιστικών Επιστημών του 2018</strong> φέτος <a href="http://dhawards.org/dhawards2018/results/">Digital \
+  Humanities Awards</a>. Ένα πελώριο ευχαριστώ σε όλους εσάς που ψηφίσατε την υποψηφιότητα του Recogito!
+
 # Tiles
-landing.index.tiles.1.title=Κοινές συλλογές
-landing.index.tiles.1.text=Ανεβάστε κείμενα και εικόνες. Δημιουργήστε τις δικές σας συλλογές. Προσκαλέστε άλλους για συνεργασία.
-landing.index.tiles.2.title=Συλλογική επισημείωση
-landing.index.tiles.2.text=Δημιουργήστε σημειώσεις και ετικέτες. Συζητήστε. Το Recogito καταγράφει \
-  την προέλευση και διατηρεί ιστορικό εκδοχών.
+landing.index.tiles.1.title=Διαμοιραζόμενες συλλογές
+landing.index.tiles.1.text=Ανεβάστε κείμενα και εικόνες. Δημιουργήστε τις δικές σας συλλογές. Προσκαλέστε \
+  άλλους για συνεργασία.
+landing.index.tiles.2.title=Συνεργατική επισημείωση
+landing.index.tiles.2.text=Δημιουργήστε σημειώσεις και ετικέτες και συζητήστε. Το Recogito καταγράφει \
+  την προέλευση και διατηρεί ιστορικό των εκδόσεων.
 landing.index.tiles.3.title=Σημασιολογικός ιστός
-landing.index.tiles.3.text=Βάλτε ετικέτες σε τόπους και ανθρώπους εύκολα με τη χρήση ελεγχόμενων \
-  λεξιλογίων και ενσωματωμένων λειτουργιών.
+landing.index.tiles.3.text=Προσθέστε ετικέτες σε τόπους, πρόσωπα και γεγονότα και φανερώστε τις μεταξύ τους σχέσεις.
 landing.index.tiles.4.title=Ανοικτά δεδομένα
-landing.index.tiles.4.text=Παρουσιάστε τα αποτελέσματα της εργασίας σας στο διαδίκτυο. Δημοσιεύστε \
-  τα δεδομένα σας ως Ανοικτά  Διασυνδεδεμένα Δεδομένα.
+landing.index.tiles.4.text=Εξάγετα τα δεδομένα σας για χρήση σε άλλες εφαρμογές. Κάντε τα αποτελέσματα της εργασίας \
+  σας περισσότερο ανοιχτά, ορατά και επαναχρησιμοποιήσιμα.
 
 # Recogito Right Now
 landing.index.right_now=Το Recogito τώρα
-landing.index.edits_last_24hrs=Τροποποιήσεις το τελευταίο 24ωρο
+landing.index.edits_last_24hrs=Τροποποιήσεις του τελευταίου 24ώρου
 
 # About
 landing.index.about=Λογισμικό ανοιχτού κώδικα του  <a href="{0}" target="_blank">Αυστριακού Τεχνολογικού \
   Ινστιτούτου</a>, του <a href="{1}" target="_blank">Exeter University</a>, του \
-  <a href="{2}" target="_blank">Ινστιτούτου Humboldt για το Διαδίκτυο και την Τεχνολογία</a>, του \
+  <a href="{2}" target="_blank">Ινστιτούτο Humboldt για το Διαδίκτυο και την Τεχνολογία</a>, του \
   <a href="{3}" target="_blank">The Open University</a> και του \
   <a href="{4}" target="_blank">University of London</a>. Με χρηματοδότηση από το \
   <a href="{5}" target="_blank">Andrew W. Mellon Foundation</a>.
@@ -79,16 +86,16 @@ account_settings.sidebar.account=Λογαριασμός
 account_settings.sidebar.password=Κωδικός πρόσβασης
 account_settings.sidebar.restore=Ανάκτηση από αντίγραφο ασφαλείας
 
-account_settings.account.account_settings=Λογαριασμός
-account_settings.account.email=Λογαριασμός ηλεκτρονικού ταχυδρομείου
+account_settings.account.account_settings=Ρυθμίσεις λογαριασμού
+account_settings.account.email=Email
 account_settings.account.name=Όνομα
-account_settings.account.about_you=Σχετικά με εσάς
+account_settings.account.about_you=Σχετικά με σας
 account_settings.account.website=Η ιστοσελίδα σας
 account_settings.account.optional=Προαιρετικό
 account_settings.account.delete.title=Διαγραφή λογαριασμού
 account_settings.account.delete.warning=Προειδοποίηση: διαγράφοντας το λογαριασμό σας θα διαγράψετε \
   και όλα τα έγγραφα και τις επισημειώσεις σας. Βεβαιωθείτε ότι έχετε αντίγραφο ασφαλείας όλων \
-  των εγγράφων που θέλετε να κρατήσετε. Η διαγραφή του λογαριασμού είναι τελεσίδικη.
+  των εγγράφων που θέλετε να κρατήσετε. Η διαγραφή του λογαριασμού είναι οριστική.
 account_settings.account.delete.button=Διαγραφή του λογαριασμού μου
 
 account_settings.password.change_password=Αλλαγή κωδικού πρόσβασης
@@ -96,46 +103,45 @@ account_settings.password.current=Τρέχων κωδικός πρόσβασης
 account_settings.password.new=Νέος κωδικός πρόσβασης
 account_settings.password.verify=Επιβεβαίωση κωδικού πρόσβασης
 
-account_settings.restore.instructions=Μπορείτε να ανακτήσετε ένα έγγραφο από μία δέσμη αρχείων ΖΙΡ \
-  που κατεβάσατε παλαιότερα χρησιμοποιώντας τη λειτουργία δημιουργίας αντιγράφων ασφαλείας \
-  του Recogito.
-account_settings.restore.button=Ανεβάστε μία δέσμη αρχείων ασφαλείας σε μορφή ΖΙΡ
+account_settings.restore.instructions=Μπορείτε να ανακτήσετε ένα έγγραφο από ένα πακέτο ΖΙΡ που \
+  κατεβάσατε παλαιότερα χρησιμοποιώντας τη λειτουργία δημιουργίας αντιγράφων ασφαλείας του Recogito.
+account_settings.restore.button=Ανεβάστε ένα αντίγραφο ασφαλείας ΖΙΡ
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Text annotation
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-document.annotation.text.toolbar.annotation_mode=ΣΕ ΛΕΙΤΟΥΡΓΊΑ ΕΠΙΣΗΜΕΊΩΣΗΣ:
-document.annotation.text.toolbar.mode.normal=ΚΑΝΟΝΙΚΌ
-document.annotation.text.toolbar.mode.quick=ΓΡΉΓΟΡΟ
+document.annotation.text.toolbar.annotation_mode=ΛΕΙΤΟΥΡΓΊΑ ΕΠΙΣΗΜΕΊΩΣΗΣ
+document.annotation.text.toolbar.mode.normal=ΚΑΝΟΝΙΚΟ
+document.annotation.text.toolbar.mode.quick=ΓΡΗΓΟΡΟ
 document.annotation.text.toolbar.mode.quick.places=Τόποι
 document.annotation.text.toolbar.mode.quick.persons=Πρόσωπα
 document.annotation.text.toolbar.mode.quick.tags=Ετικέτες
-document.annotation.text.toolbar.mode.relations=RELATIONS
-document.annotation.text.toolbar.color=ΧΡΏΜΑ:
-document.annotation.text.toolbar.color.by_type=ΑΝΆ ΤΎΠΟ ΟΝΤΌΤΗΤΑΣ
-document.annotation.text.toolbar.color.by_status=ΑΝΆ ΚΑΤΆΣΤΑΣΗ ΕΠΙΒΕΒΑΊΩΣΗΣ
-document.annotation.text.toolbar.color.by_tag=ΑΝΆ ΕΤΙΚΈΤΑ
+document.annotation.text.toolbar.mode.relations=ΣΧΕΣΕΙΣ
+document.annotation.text.toolbar.color=ΧΡΩΜΑ
+document.annotation.text.toolbar.color.by_type=ΑΝΑ ΤΥΠΟ ΟΝΤΟΤΗΤΑΣ
+document.annotation.text.toolbar.color.by_status=ΑΝΑ ΚΑΤΑΣΤΑΣΗ ΕΠΙΒΕΒΑΙΩΣΗΣ
+document.annotation.text.toolbar.color.by_tag=ΜΕ ΤΗΝ ΠΡΩΤΗ ΕΤΙΚΕΤΑ
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Image annotation
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-document.annotation.image.toolbar.tools=Εργαλεία
-document.annotation.image.toolbar.tools.move=Μετακίνηση
-document.annotation.image.toolbar.tools.point=Σημείο
-document.annotation.image.toolbar.tools.rectangle=Ορθογώνιο
-document.annotation.image.toolbar.tools.tilted_box=Κεκλιμένο κουτί
+document.annotation.image.toolbar.tools=ΕΡΓΑΛΕΙΑ
+document.annotation.image.toolbar.tools.move=ΜΕΤΑΚΙΝΗΣΗ
+document.annotation.image.toolbar.tools.point=ΣΗΜΕΙΟ
+document.annotation.image.toolbar.tools.rectangle=ΟΡΘΟΓΩΝΙΟ
+document.annotation.image.toolbar.tools.tilted_box=ΚΕΚΛΙΜΕΝΟ ΟΡΘΟΓΩΝΙΟ
 
-document.annotation.image.toolbar.image=Εικόνα
-document.annotation.image.toolbar.layers=Επίπεδα
-document.annotation.image.toolbar.help=Βοήθεια
+document.annotation.image.toolbar.image=ΕΙΚΟΝΑ
+document.annotation.image.toolbar.layers=ΣΤΡΩΣΕΙΣ
+document.annotation.image.toolbar.help=ΒΟΗΘΕΙΑ
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Downloads
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-document.downloads.annotations.csv.text=Κατεβάστε τις επισημειώσεις με τη μορφή πίνακα, για να τον εισάγετε σε λογισμικό υπολογιστικών φύλλων ή σε GIS.
+document.downloads.annotations.csv.text=Κατεβάστε τις επισημειώσεις με μορφή πίνακα, για να τον εισάγετε σε υπολογιστικό φύλλο ή εφαρμογή GIS.
 document.downloads.annotations.rdf.text=Κατεβάστε τις επισημειώσεις και τα μεταδεδομένα εγγράφων με μορφή RDF, κωδικοποιημένα με τη χρήση των προτύπων Open Annotation και Dublin Core.
-document.downloads.annotations.geojson.text=Επιβεβαιωμένες γεωχωρικά εντοπισμένες τοποθεσίες στο έγγραφο σε μορφή GeoJSON FeatureCollection.
-document.downloads.annotations.kml.text=Επιβεβαιωμένες γεωχωρικά εντοπισμένες τοποθεσίες σε μορφή αρχείου KML, για θέαση στο Google Earth.
+document.downloads.annotations.geojson.text=Επιβεβαιωμένες γεωχωρικά εντοπισμένες τοποθεσίες στο έγγραφο με μορφή GeoJSON FeatureCollection.
+document.downloads.annotations.kml.text=Επιβεβαιωμένες γεωχωρικά εντοπισμένες τοποθεσίες με μορφή αρχείου KML, για προβολή στο Google Earth.
 document.downloads.annotations.tei.text=Επισημειωμένο κείμενο σε βασική TEI/XML κωδικοποίηση. \
   Σημείωση: μέχρι στιγμής περιλαμβάνονται μόνο επισημειώσεις τοπωνυμίων. Επίσης, οι \
   αλληλοεπικαλυπτόμενες επισημειώσεις αφαιρούνται καθώς το TEI δεν τις υποστηρίζει.
@@ -144,26 +150,32 @@ document.downloads.annotations.tei.text=Επισημειωμένο κείμεν�
 # Document settings
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 document.settings.sidebar.metadata=Μεταδεδομένα
+document.settings.sidebar.preferences=Προτιμήσεις επισημείωσης
 document.settings.sidebar.sharing=Διαμοιρασμός
-document.settings.sidebar.edit_history=Ιστορικό επεξεργασίας
-document.settings.sidebar.backup=Δημιουργία αντιγράφου ασφαλείας
+document.settings.sidebar.edit_history=Επεξεργασία ιστορικού
+document.settings.sidebar.backup=Αντίγραφο ασφαλείας
 document.settings.sidebar.delete=Διαγραφή
 
 # Metadata
 document.settings.metadata.document_metadata=Μεταδεδομένα εγγράφου
 document.settings.metadata.document_parts=Μέρη εγγράφου
 document.settings.metadata.edit_part=Επεξεργασία επιμέρους μεταδεδομένων
-document.settings.metadata.drag_to_change=Σύρατε για να αλλάξετε σειρά
+document.settings.metadata.drag_to_change=Σύρετε για να αλλάξετε σειρά
 
 # Sharing
 document.settings.sharing.public_access=Δημόσια πρόσβαση
 document.settings.sharing.visible_to_all=Ορατό από όλους
 
+document.settings.sharing.collaborators=Συνεργάτες
+document.settings.sharing.collaborators.none=Γι' αυτό το έγγραφο δεν υπάρχουν ακόμη συνεργάτες. \
+  Χρησιμοποιήστε την παρακάτω φόρμα για να προσθέσετε συνεργάτες.
+document.settings.sharing.collaborators.enter_username=Εισαγωγή ονόματος χρήστη
+
 # Backup
-document.settings.backup.backup=Δημιουργία αντιγράφου ασφαλείας
-document.settings.backup.description=Η δέσμη αρχείων ασφαλείας περιλαμβάνει τα μεταδεδομένα του \
+document.settings.backup.backup=Αντίγραφο ασφαλείας
+document.settings.backup.description=Το αντίγραφο ασφαλείας περιλαμβάνει τα μεταδεδομένα του \
   εγγράφου, τα αρχεία και τις επισημειώσεις. Μπορείτε να το χρησιμοποιήσετε για να ανακτήσετε \
-  την εργασία σας στο Recogito σε κατοπινό χρόνο. Δημιουργείτε τακτικά δέσμη αρχείων \
-  ασφαλείας για επιπλέον ασφάλεια. Σημείωση: το ιστορικό επεξεργασίας δεν περιλαμβάνεται προς \
-  το παρόν στη δέσμη αρχείων ασφαλείας.
-document.settings.backup.button=Κατεβάστε μία δέσμη αρχείων ασφαλείας ΖΙΡ
+  την εργασία σας στο Recogito σε επόμενο χρόνο. Δημιουργείτε τακτικά αντίγραφα ασφαλείας \
+  για επιπλέον ασφάλεια. Παρακαλώ σημειώστε ότι το <u>ιστορικό επεξεργασίας δεν περιλαμβάνεται</u> \
+  στο αντίγραφο ασφαλείας.
+document.settings.backup.button=Λήψη αντιγράφο ασφαλείας ΖΙΡ
